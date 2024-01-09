8f4e1137d096e2429971bfb6103fd76b8ee349a0
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-01-09 14:37:11 +0800

    Update main.cpp

--- code/win/1-SDL/05-sdl-yuv/main.cpp
+++ code/win/1-SDL/05-sdl-yuv/main.cpp
@@ -1,56 +1,38 @@
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <fstream>
+#include <thread>
 #include <memory_resource>
 #include <SDL.h>
 
 #undef main
 
 using namespace std;
-
-//自定义消息类型
-constexpr auto REFRESH_EVENT  {SDL_USEREVENT + 1};     // 请求画面刷新事件
-constexpr auto QUIT_EVENT  {SDL_USEREVENT + 2};    // 退出事件
-
-//定义分辨率
-// YUV像素分辨率
-constexpr uint32_t YUV_WIDTH {320};
-constexpr uint32_t YUV_HEIGHT  {240};
-//定义YUV格式
-constexpr auto YUV_FORMAT {SDL_PIXELFORMAT_IYUV};
-
-int s_thread_exit {};  // 退出标志 = 1则退出
-
-int refresh_video_timer(void *data)
-{
-    (void)data;
-    while (!s_thread_exit){
-        SDL_Event event;
-        event.type = REFRESH_EVENT;
-        SDL_PushEvent(&event);
-        SDL_Delay(50);
-    }
-
-    s_thread_exit = 0;
-
-    //push quit event
-    SDL_Event event;
-    event.type = QUIT_EVENT;
-    SDL_PushEvent(&event);
-    return 0;
-}
+using namespace chrono;
+using namespace this_thread;
 
 int main()
 {
+    //自定义消息类型
+    constexpr auto REFRESH_EVENT  {SDL_USEREVENT + 1};     // 请求画面刷新事件
+    constexpr auto QUIT_EVENT  {SDL_USEREVENT + 2};    // 退出事件
+
+    //定义分辨率
+    // YUV像素分辨率
+    constexpr uint32_t YUV_WIDTH {320},YUV_HEIGHT{240};
+
+    //定义YUV格式
+    constexpr auto YUV_FORMAT {SDL_PIXELFORMAT_IYUV};
+
     //初始化 SDL
     if(SDL_Init(SDL_INIT_VIDEO)){
         throw string("Could not initialize SDL - ") + SDL_GetError();
     }
 
-    // 分辨率
+    //分辨率
     // 1. YUV的分辨率
     constexpr auto video_width {YUV_WIDTH},
-                        video_height {YUV_HEIGHT};
+                    video_height {YUV_HEIGHT};
 
     // 2.显示窗口的分辨率
     auto win_width {YUV_WIDTH},win_height {YUV_WIDTH};
@@ -64,14 +46,14 @@ int main()
         throw string("SDL: could not create window, err : ") + SDL_GetError() + "\n";
     }
 
-    // 基于窗口创建渲染器
+    //基于窗口创建渲染器
     auto renderer {SDL_CreateRenderer(window, -1, 0)};
     if(!renderer){
         SDL_DestroyWindow(window);
         throw string("SDL: could not create Renderer, err : ") + SDL_GetError() + "\n";
     }
 
-    // 基于渲染器创建纹理
+    //基于渲染器创建纹理
     auto texture { SDL_CreateTexture(renderer,
                           YUV_FORMAT, // YUV420P，即是SDL_PIXELFORMAT_IYUV
                           SDL_TEXTUREACCESS_STREAMING,
@@ -88,7 +70,7 @@ int main()
     std::pmr::pool_options opt{};
     std::pmr::synchronized_pool_resource mpool(opt);
 
-    // 我们测试的文件是YUV420P格式
+    //测试的文件是YUV420P格式
     constexpr auto y_frame_len {video_width * video_height},
     u_frame_len {y_frame_len / 4},v_frame_len {y_frame_len / 4},
     yuv_frame_len {y_frame_len + u_frame_len + v_frame_len};
@@ -96,27 +78,44 @@ int main()
     void *video_buf{};
     try {
         video_buf = mpool.allocate(yuv_frame_len);
-    } catch (const string &e) {
+    } catch (const bad_alloc &e) {
         SDL_DestroyWindow(window);
         SDL_DestroyRenderer(renderer);
         SDL_DestroyTexture(texture);
-        throw e + " Failed to alloce yuv frame space!\n";
+        const string errmsg(e.what());
+        cerr << errmsg;
+        throw errmsg + " Failed to alloce yuv frame space!\n";
     }
 
     // 打开YUV文件
     constexpr auto yuv_path {"yuv420p_320x240.yuv"};
-    auto video_fd { fopen(yuv_path, "rb")};/*只读和二进制形式打开*/
-    if(!video_fd ){
+    ifstream ifs(yuv_path,ios::binary); /*只读和二进制形式打开*/
+    if(!ifs){
         SDL_DestroyWindow(window);
         SDL_DestroyRenderer(renderer);
         SDL_DestroyTexture(texture);
+        mpool.deallocate(video_buf,yuv_frame_len);
         mpool.release();
         cerr << "Failed to open yuv file\n";
         throw "Failed to open yuv file\n";
     }
 
     // 创建请求刷新线程
-    auto timer_thread { SDL_CreateThread(refresh_video_timer, nullptr, nullptr)};
+    bool s_thread_exit {};  // 退出标志 = true则退出
+    thread timer_thread([&](){
+
+        while (!s_thread_exit){
+            SDL_Event event{.type = REFRESH_EVENT};
+            SDL_PushEvent(&event);
+            sleep_for(50ms);
+        }
+
+        s_thread_exit = false;
+
+        //push quit event
+        SDL_Event event{.type = QUIT_EVENT};
+        SDL_PushEvent(&event);
+    });
 
     for(;;){
 
@@ -124,12 +123,24 @@ int main()
         SDL_Event event{};
         SDL_WaitEvent(&event);
 
-        if(event.type == REFRESH_EVENT){ // 画面刷新事件
+        if(REFRESH_EVENT == event.type){ // 画面刷新事件
 
-            auto video_buff_len {fread(video_buf, 1, yuv_frame_len, video_fd)}; /*一次读取一帧*/
-            if(video_buff_len <= 0){
+            //auto video_buff_len {fread(video_buf, 1, yuv_frame_len, video_fd)}; /*一次读取一帧*/
+
+            try {
+                ifs.read(static_cast<char*>(video_buf),yuv_frame_len);
+            } catch (const ios::failure &e) {
+                cerr << e.what() << "\n";
                 cerr << "Failed to read data from yuv file!\n";
+                cerr << SDL_GetError();
                 goto _FAIL;
+            }
+
+            const auto video_buff_len {ifs.gcount()};
+
+            if(!video_buff_len){
+                cout << "Play finish\n" << flush;
+                break;
             }
             // 设置纹理的数据 video_width = 320, plane
             /*采用planar格式存储,4个Y,对应一个U和一个V*/
@@ -165,36 +176,28 @@ int main()
             SDL_GetWindowSize(window, w, h);/*支持拉伸操作*/
             cout << "SDL_WINDOWEVENT win_width : " << *w << ", win_height: " << *h << "\n";
         }else if(SDL_QUIT == event.type){ //退出事件
-            s_thread_exit = 1;
+            s_thread_exit = true;
         }else if(QUIT_EVENT == event.type){
+            cout << "QUIT_EVENT\n";
             break;
         }else{}
     }
 
 _FAIL:
-    s_thread_exit = 1;      // 保证线程能够退出
-    // 释放资源
-    if(timer_thread){
-        SDL_WaitThread(timer_thread, nullptr); // 等待线程退出
-    }
+    s_thread_exit = true;      // 保证线程能够退出
 
+    timer_thread.join(); // 等待线程退出
+
+    ifs.close();
+    // 释放资源
+    mpool.deallocate(video_buf,yuv_frame_len);
     mpool.release();
 
-    if(video_fd){
-        fclose(video_fd);
-    }
+    SDL_DestroyTexture(texture);
 
-    if(texture){
-        SDL_DestroyTexture(texture);
-    }
+    SDL_DestroyRenderer(renderer);
 
-    if(renderer){
-        SDL_DestroyRenderer(renderer);
-    }
-
-    if(window){
-        SDL_DestroyWindow(window);
-    }
+    SDL_DestroyWindow(window);
 
     SDL_Quit();
 
