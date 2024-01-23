4010db5909823e2353866e9753ca584be94407f6
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-01-23 11:14:21 +0800

    01-buffer

---
+++ code/win/2-FFmpeg/01-buffer/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+extern "C"{
+#include "libavutil/avutil.h"
+#include "libavformat/avformat.h"
+}
+
+using namespace std;
+
+int main(int argc,const char* argv[])
+{
+
+
+
+    return 0;
+}
