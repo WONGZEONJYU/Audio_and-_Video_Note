926f564ffc174079ae14e27c0092408d2e473d51
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-03-06 17:55:10 +0800

    Update main.cpp

--- code/win/2-FFmpeg/06-decode_audio/main.cpp
+++ code/win/2-FFmpeg/06-decode_audio/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <bits/ranges_algobase.h>
+
 extern "C" {
 #include <libavutil/frame.h>
 #include <libavutil/mem.h>
@@ -24,11 +26,11 @@ static void print_sample_format(const AVFrame *const frame)
 {
     std::cout << "ar-samplerate: " << frame->sample_rate << "Hz\n";
     std::cout << "ac-channel: "<< frame->ch_layout.nb_channels << "\n";
-    std::cout << "f-format: " << frame->format << "\n";
+    std::cout << "f-format: " << frame->format << " " << av_get_sample_fmt_name(static_cast<AVSampleFormat>(frame->format)) << "\n";
     // 格式需要注意,实际存储到本地文件时已经改成交错模式
 }
 
-static void decode(AVCodecContext *dec_ctx, AVPacket *pkt, AVFrame *frame,
+static void decode(AVCodecContext *dec_ctx,const AVPacket *pkt, AVFrame *frame,
                    ofstream& outfile)
 {
     /* send the packet with the compressed data to the decoder */
@@ -52,6 +54,7 @@ static void decode(AVCodecContext *dec_ctx, AVPacket *pkt, AVFrame *frame,
             std::cerr << "Error during decoding\n";
             exit(ret);
         }
+        //std::cout << av_get_sample_fmt_name(dec_ctx->sample_fmt) << "\n";
         const auto data_size {av_get_bytes_per_sample(dec_ctx->sample_fmt)};
         if (data_size < 0){
             /* This should not occur, checking just for paranoia */
@@ -79,6 +82,14 @@ static void decode(AVCodecContext *dec_ctx, AVPacket *pkt, AVFrame *frame,
     }
 }
 
+template<typename T>
+struct destory_ final{
+    explicit destory_(T&& f):f(std::move(f)){}
+    ~destory_(){f();}
+private:
+    T f;
+};
+
 // 播放范例: ffplay -ar 48000 -ac 2 -f f32le believe.pcm
 int main(const int argc,const char* argv[])
 {
@@ -89,6 +100,24 @@ int main(const int argc,const char* argv[])
 
     const auto filename {argv[1]};
     const auto outfilename {argv[2]};
+    AVPacket* pkt{};
+    AVCodecParserContext *parser{};
+    AVCodecContext *codec_ctx{};
+    AVFrame *decoded_frame{};
+    ifstream in_file;
+    ofstream out_file;
+
+    auto destory{[&](){
+        std::cout << "destory()\n";
+        in_file.close();
+        out_file.close();
+        av_parser_close(parser);
+        avcodec_free_context(&codec_ctx);
+        av_frame_free(&decoded_frame);
+        av_packet_free(&pkt);
+    }};
+
+    destory_ d(std::move(destory));
 
     auto audio_codec_id {AV_CODEC_ID_AAC};
 
@@ -108,50 +137,86 @@ int main(const int argc,const char* argv[])
     }
 
     // 获取裸流的解析器 AVCodecParserContext(数据)  +  AVCodecParser(方法)
-    auto parser {av_parser_init(codec->id)};
+    parser = av_parser_init(codec->id);
     if (!parser) {
         std::cerr << "Parser not found\n";
         return -1;
     }
 
     // 分配codec上下文
-    auto codec_ctx { avcodec_alloc_context3(codec)};
+    codec_ctx = avcodec_alloc_context3(codec);
     if (!codec_ctx) {
-        av_parser_close(parser);
         std::cout << "Could not allocate audio codec context\n";
         return -1;
     }
 
     if(avcodec_open2(codec_ctx,codec,nullptr) < 0){
         std::cerr << "Could not open codec\n";
-        av_parser_close(parser);
-        avcodec_free_context(&codec_ctx);
         return -1;
     }
 
     /*open input file*/
-    ifstream in_file(filename,ios::binary);
+    in_file.open(filename,ios::binary);
     if (!in_file){
         std::cerr << "open input file faild\n";
-        av_parser_close(parser);
-        avcodec_free_context(&codec_ctx);
-        avcodec_free_context(&codec_ctx);
+        return -1;
+    }
+    /*open output file*/
+    out_file.open(outfilename,ios::binary);
+    if (!out_file){
+        std::cerr << "open output file faild\n";
         return -1;
     }
 
-    auto pkt {av_packet_alloc()};
+    pkt = av_packet_alloc();
+    if (!pkt){
+        std::cerr << "av_packet_alloc faild\n";
+        return -1;
+    }
+    decoded_frame = av_frame_alloc();
+    if (!decoded_frame){
+        std::cerr << "Could not allocate audio frame\n";
+        return -1;
+    }
 
-    struct _release{
-        ~_release(){
-            av_parser_close(parser);
-            avcodec_free_context(&codec_ctx);
-            av_packet_free(&pkt);
+    uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE]{};
+    auto data{inbuf};
+    in_file.read(reinterpret_cast<char*>(inbuf),AUDIO_INBUF_SIZE);
+    auto read_data_size{in_file.tellg()};
+
+    while (read_data_size > 0){
+
+        const auto ret {av_parser_parse2(parser, codec_ctx, &pkt->data, &pkt->size,
+                               data, read_data_size,AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0) };
+        if (ret < 0){
+            std::cerr << "Error while parsing\n";
+            return -1;
         }
-    };
 
-    // av_parser_close(parser);
-    // avcodec_free_context(&codec_ctx);
-    // av_packet_free(&pkt);
+        data += ret;
+        read_data_size -= ret;
 
+        if (pkt->size){
+            decode(codec_ctx,pkt,decoded_frame,out_file);
+        }
+
+        if (read_data_size < AUDIO_REFILL_THRESH){
+            //std::move(data,data + read_data_size,inbuf);
+            //memmove(inbuf,data,read_data_size);
+            data = inbuf;
+            in_file.read(reinterpret_cast<char*>(data + read_data_size),AUDIO_REFILL_THRESH - read_data_size);
+            const auto len{in_file.tellg()};
+            //read_data_size = len > 0 ? read_data_size + len : read_data_size;
+            if (len > 0){
+                read_data_size += len;
+            }
+        }
+    }
+
+    pkt->data = nullptr;
+    pkt->size = 0;
+    decode(codec_ctx,pkt,decoded_frame,out_file);
+
+    std::cout << "main finish, please enter Enter and exit\n";
     return 0;
 }
