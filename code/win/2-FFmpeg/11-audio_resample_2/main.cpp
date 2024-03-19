d64af9e722f9adeddeba4f0fe09e2424250fc669
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-03-19 17:20:50 +0800

    add audio_resample_2 project

---
+++ code/win/2-FFmpeg/11-audio_resample_2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+extern "C"
+{
+#include <libavutil/opt.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/samplefmt.h>
+#include <libswresample/swresample.h>
+}
+
+template<typename F>
+struct Destroyer final{
+    Destroyer(const Destroyer&) = delete;
+    Destroyer& operator=(const Destroyer&) = delete;
+    explicit Destroyer(F &&f):fn(std::move(f)){}
+    ~Destroyer() {
+        fn();
+    }
+private:
+    const F fn;
+};
+
+static std::string av_get_err(const int& errnum)
+{
+    constexpr auto ERROR_STRING_SIZE {1024};
+    char err_buf[ERROR_STRING_SIZE]{};
+    av_strerror(errnum, err_buf, std::size(err_buf));
+    return {err_buf};
+}
+
+int main(const int argc,const char* argv[])
+{
+
+    return 0;
+}
