15b0234c0e748483c0e76a21859703777776631d
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-03-27 15:21:18 +0800

    add encode_audio

---
+++ code/win/2-FFmpeg/12-encode_audio/main.cpp
@@ -0,0 +1,21 @@
+
+extern "C"{
+#include <libavcodec/codec.h>
+#include <libavformat/avformat.h>
+}
+
+#include <iostream>
+
+
+
+
+
+int main(const int argc,const char* argv[])
+{
+
+
+
+
+
+    return 0;
+}
