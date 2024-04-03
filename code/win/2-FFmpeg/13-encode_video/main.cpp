955732cad8d1b784e5188a5693642493e46256f3
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-04-03 15:25:15 +0800

    Update main.cpp

--- code/win/2-FFmpeg/13-encode_video/main.cpp
+++ code/win/2-FFmpeg/13-encode_video/main.cpp
@@ -10,6 +10,7 @@ extern "C"{
 #include <fstream>
 #include <memory_resource>
 
+#if 1
 template<typename F>
 struct Destroyer final{
     Destroyer(const Destroyer&) = delete;
@@ -22,6 +23,13 @@ private:
     const F fn;
 };
 
+static auto get_now_time(){
+    const auto now {std::chrono::high_resolution_clock::now()};
+    const auto duration {now.time_since_epoch()};
+    const auto microseconds {std::chrono::duration_cast<std::chrono::microseconds>(duration)};
+    return microseconds.count() / 1000;
+}
+
 static std::string av_get_err(const int& errnum)
 {
     constexpr auto ERROR_STRING_SIZE {1024};
@@ -32,21 +40,26 @@ static std::string av_get_err(const int& errnum)
 
 static int encode(AVCodecContext &context,const AVFrame &frame,AVPacket &packet,std::ofstream &out_file){
 
+    std::cout <<__FUNCTION__  << " begin\n";
+
     if (frame.extended_data){
         std::cout << "Send frame " << frame.pts << "\n";
     }
 
-    auto ret {frame.extended_data ? avcodec_send_frame( &context,&frame) :avcodec_send_frame( &context, nullptr)};
+    auto ret {frame.extended_data ? avcodec_send_frame( &context,&frame) : avcodec_send_frame( &context, nullptr)};
     if (ret < 0){
         std::cerr << "avcodec_send_frame failed : " << ret << " " << av_get_err(ret) << "\n";
         return -1;
     }
 
     for (;;) {
+
         ret = avcodec_receive_packet(&context,&packet);
         if (AVERROR_EOF == ret || AVERROR(EAGAIN) == ret){
+            std::cout << __FUNCTION__  << " end\n";
             return 0;
         }else if(ret < 0){
+            std::cerr << __FUNCTION__  << " end\n";
             std::cerr << "avcodec_receive_packet failed : " << ret << " " << av_get_err(ret) << "\n";
             return ret;
         }else{}
@@ -61,7 +74,7 @@ static int encode(AVCodecContext &context,const AVFrame &frame,AVPacket &packet,
 
         out_file.write(reinterpret_cast<const char* >(packet.data),packet.size);
     }
-    return 0;
+
 }
 
 int main(const int argc,const char *argv[]) {
@@ -131,19 +144,21 @@ int main(const int argc,const char *argv[]) {
     avCodecContext->pix_fmt = AV_PIX_FMT_YUV420P;
 
     if (AV_CODEC_ID_H264 == codec->id){
-
+        //相关的参数可以参考libx264.c的 AVOption options
         if (av_opt_set(avCodecContext->priv_data, "preset", "medium", 0) < 0){
+            /*调整编码速度*/
             std::cerr << "av_opt_set preset failed\n";
         }
 
         if (av_opt_set(avCodecContext->priv_data, "profile", "main", 0) < 0){
+            /*调整画质*/
             std::cerr << "av_opt_set profile failed\n";
         }
 
         if (av_opt_set(avCodecContext->priv_data, "tune","zerolatency",0) < 0){
+            /*视觉优化*/
             std::cerr << "av_opt_set tune failed\n";
         }
-
     }
 
     /*设置编码器参数*/
@@ -161,7 +176,10 @@ int main(const int argc,const char *argv[]) {
      *  不设置则每个I帧都带 sps pps sei
      */
 
-//    avCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; // 存本地文件时不要去设置
+  // avCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; // 存本地文件时不要去设置
+  /*Place global headers in extradata instead of every keyframe.
+   * 将全局头信息放在extradata中,而不是每个关键帧中*/
+
     auto ret{avcodec_open2(avCodecContext,codec, nullptr)};
 
     if (ret < 0){
@@ -193,22 +211,75 @@ int main(const int argc,const char *argv[]) {
         std::cerr << "Could not allocate the video frame data " << av_get_err(ret) << "\n";
         return -1;
     }
-
+    //计算出每一帧的数据 像素格式 * 宽 * 高
     one_frame_size = av_image_get_buffer_size(avCodecContext->pix_fmt,avCodecContext->width,avCodecContext->height,1);
     if (one_frame_size < 0){
         std::cerr << "av_image_get_buffer_size failed : " << av_get_err(one_frame_size) << "\n";
         return -1;
     }
 
-    one_frame_buf = static_cast<uint8_t*>(mptool.allocate(one_frame_size));
-    if (!one_frame_buf){
-        std::cerr << "yuv_buf malloc failed\n";
+    try {
+        one_frame_buf = static_cast<uint8_t*>(mptool.allocate(one_frame_size));
+    } catch (std::exception &e) {
+        std::cerr << "yuv_buf malloc failed : " << e.what() << "\n";
         return -1;
     }
 
+    const auto all_begin_time{get_now_time()};
+    int64_t pts{};
     for(;;){
+        std::fill_n(one_frame_buf,one_frame_size,0);
+        try {
+            in_yuv_file.read(reinterpret_cast<char*>(one_frame_buf),one_frame_size);
+            if (in_yuv_file.eof()){
+                break;
+            }
+        } catch (std::exception &e) {
+            std::cerr << "in_yuv_file read error : " << e.what() << "\n";
+            return -1;
+        }
 
+        /*
+         * 确保该frame可写,如果编码器内部保持了内存参考计数,则需要重新拷贝一个备份
+            目的是新写入的数据和编码器保存的数据不能产生冲突
+        */
+
+        ret = av_frame_make_writable(frame);
+        if (ret < 0){
+            std::cerr << "av_frame_make_writable failed : " << ret << " " << av_get_err(ret) << "\n";
+            return -1;
+        }
+
+        const auto need_size{av_image_fill_arrays(frame->data,frame->linesize,
+                                                  one_frame_buf,static_cast<AVPixelFormat>(frame->format),
+                                                  frame->width,frame->height,1)};
+        if (need_size != one_frame_size){
+            std::cerr << "av_image_fill_arrays failed , needsize : " << need_size << " ,one_frame_size : " << one_frame_size << "\n";
+            break;
+        }
+
+        pts += 40; /* 1 /40 = 0.025 == 25帧*/
+        frame->pts = pts;
+
+        const auto encode_begin_time{get_now_time()};
+        ret = encode(*avCodecContext,*frame,*packet,out_h264_file);
+        const auto encode_end_time{get_now_time() - encode_begin_time};
+        std::cout << "encode time cost: " << encode_end_time << "ms\n";
+        if (ret < 0){
+            break;
+        }
     }
+
+    const auto flush_begin_time{get_now_time()};
+    encode(*avCodecContext,{},*packet,out_h264_file);
+    const auto flush_end_time{get_now_time() - flush_begin_time};
+    std::cout << "flush encode time cost : " << flush_end_time << "\n";
+
+    const auto all_end_time{get_now_time() - all_begin_time};
+    std::cout << "all time cost : " << all_end_time << "ms\n" << "encode finsh\n";
 
     return 0;
 }
+#endif
+
+
