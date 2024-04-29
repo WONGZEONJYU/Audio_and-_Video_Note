224fbcac39054200b0fef671ee7df67ad6b302ba
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-04-29 18:12:35 +0800

    Update main.cpp

--- code/win/2-FFmpeg/16-video-filter/main.cpp
+++ code/win/2-FFmpeg/16-video-filter/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "AVFilter_Demo.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(const int argc,const char *argv[]) {
+
+    if (argc < 3){
+        std::cerr << "usage: in.yuv out.yuv";
+        return -1;
+    }
+
+    try {
+        auto filter{new_AVFilter_Demo(argv[1],argv[2])};
+        filter->exec();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << "\n";
+        return -1;
+    }
+
     return 0;
 }
+
