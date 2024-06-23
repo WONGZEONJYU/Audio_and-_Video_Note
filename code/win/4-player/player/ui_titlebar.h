cbd94cb255455d9c19fa4557d8130d989bac165c
fuck_if <wongjy13005151320@outlook.com>
2024-06-23 23:24:05 +0800

    Update ui_titlebar.h

--- code/win/4-player/player/ui_titlebar.h
+++ code/win/4-player/player/ui_titlebar.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'titlebar.ui'
 **
-** Created by: Qt User Interface Compiler version 6.4.2
+** Created by: Qt User Interface Compiler version 6.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -11,9 +11,10 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -21,9 +22,10 @@ QT_BEGIN_NAMESPACE
 class Ui_TitleBar
 {
 public:
-    QGridLayout *gridLayout;
+    QHBoxLayout *horizontalLayout;
     QPushButton *MenuBtn;
     QLabel *MovieNameLab;
+    QSpacerItem *horizontalSpacer;
     QPushButton *MinBtn;
     QPushButton *MaxBtn;
     QPushButton *FullScreenBtn;
@@ -33,58 +35,63 @@ public:
     {
         if (TitleBar->objectName().isEmpty())
             TitleBar->setObjectName("TitleBar");
-        TitleBar->resize(826, 50);
-        TitleBar->setMaximumSize(QSize(16777215, 50));
-        gridLayout = new QGridLayout(TitleBar);
-        gridLayout->setSpacing(0);
-        gridLayout->setObjectName("gridLayout");
-        gridLayout->setContentsMargins(0, 0, 0, 0);
+        TitleBar->resize(780, 60);
+        TitleBar->setMinimumSize(QSize(0, 60));
+        TitleBar->setMaximumSize(QSize(16777215, 60));
+        horizontalLayout = new QHBoxLayout(TitleBar);
+        horizontalLayout->setSpacing(0);
+        horizontalLayout->setObjectName("horizontalLayout");
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
         MenuBtn = new QPushButton(TitleBar);
         MenuBtn->setObjectName("MenuBtn");
-        MenuBtn->setMinimumSize(QSize(200, 33));
-        MenuBtn->setMaximumSize(QSize(200, 33));
+        MenuBtn->setMinimumSize(QSize(160, 50));
+        MenuBtn->setMaximumSize(QSize(160, 16777215));
         QFont font;
         font.setFamilies({QString::fromUtf8("Bahnschrift Light SemiCondensed")});
         font.setPointSize(18);
         MenuBtn->setFont(font);
 
-        gridLayout->addWidget(MenuBtn, 0, 0, 1, 1);
+        horizontalLayout->addWidget(MenuBtn);
 
         MovieNameLab = new QLabel(TitleBar);
         MovieNameLab->setObjectName("MovieNameLab");
-        MovieNameLab->setMinimumSize(QSize(426, 50));
-        MovieNameLab->setMaximumSize(QSize(426, 50));
+        MovieNameLab->setMinimumSize(QSize(420, 50));
+        MovieNameLab->setMaximumSize(QSize(16777215, 50));
         MovieNameLab->setWordWrap(false);
 
-        gridLayout->addWidget(MovieNameLab, 0, 1, 1, 1);
+        horizontalLayout->addWidget(MovieNameLab);
+
+        horizontalSpacer = new QSpacerItem(45, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
 
         MinBtn = new QPushButton(TitleBar);
         MinBtn->setObjectName("MinBtn");
         MinBtn->setMinimumSize(QSize(50, 50));
         MinBtn->setMaximumSize(QSize(50, 50));
 
-        gridLayout->addWidget(MinBtn, 0, 2, 1, 1);
+        horizontalLayout->addWidget(MinBtn);
 
         MaxBtn = new QPushButton(TitleBar);
         MaxBtn->setObjectName("MaxBtn");
         MaxBtn->setMinimumSize(QSize(50, 50));
         MaxBtn->setMaximumSize(QSize(50, 50));
 
-        gridLayout->addWidget(MaxBtn, 0, 3, 1, 1);
+        horizontalLayout->addWidget(MaxBtn);
 
         FullScreenBtn = new QPushButton(TitleBar);
         FullScreenBtn->setObjectName("FullScreenBtn");
         FullScreenBtn->setMinimumSize(QSize(50, 50));
         FullScreenBtn->setMaximumSize(QSize(50, 50));
 
-        gridLayout->addWidget(FullScreenBtn, 0, 4, 1, 1);
+        horizontalLayout->addWidget(FullScreenBtn);
 
         CloseBtn = new QPushButton(TitleBar);
         CloseBtn->setObjectName("CloseBtn");
         CloseBtn->setMinimumSize(QSize(50, 50));
         CloseBtn->setMaximumSize(QSize(50, 50));
 
-        gridLayout->addWidget(CloseBtn, 0, 5, 1, 1);
+        horizontalLayout->addWidget(CloseBtn);
 
 
         retranslateUi(TitleBar);
