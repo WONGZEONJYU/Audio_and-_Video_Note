4ab27d5591e073cc79e2fa6e1516e5e75f18a1e6
fuck_if <wongjy13005151320@outlook.com>
2024-06-23 23:24:09 +0800

    Update ui_MainWindow.h

--- code/win/4-player/player/ui_MainWindow.h
+++ code/win/4-player/player/ui_MainWindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 6.4.2
+** Created by: Qt User Interface Compiler version 6.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -18,6 +18,8 @@
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 #include <ctrlbar.hpp>
+#include <displaybox.hpp>
+#include <playlistbox.hpp>
 #include <titlebar.hpp>
 
 QT_BEGIN_NAMESPACE
@@ -29,12 +31,12 @@ public:
     QGridLayout *gridLayout;
     QWidget *showCtrlBarBgWidget;
     QGridLayout *gridLayout_2;
-    QWidget *ShowWidget;
     CtrlBar *CtrlBarWidget;
+    DisplayBox *ShowWidget;
     QMenuBar *menubar;
     QStatusBar *statusbar;
     QDockWidget *PlaylistWidget;
-    QWidget *PlaylistContents;
+    PlayListBox *PlaylistContents;
     QDockWidget *TitleWidget_3;
     TitleBar *TitleContents_2;
 
@@ -42,32 +44,37 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 500);
+        MainWindow->resize(1281, 909);
+        MainWindow->setMinimumSize(QSize(1280, 909));
+        MainWindow->setMaximumSize(QSize(16777215, 16777215));
         MainWindow->setAnimated(true);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        centralwidget->setMinimumSize(QSize(1080, 740));
         gridLayout = new QGridLayout(centralwidget);
         gridLayout->setSpacing(0);
         gridLayout->setObjectName("gridLayout");
         gridLayout->setContentsMargins(0, 0, 0, 0);
         showCtrlBarBgWidget = new QWidget(centralwidget);
         showCtrlBarBgWidget->setObjectName("showCtrlBarBgWidget");
+        showCtrlBarBgWidget->setMinimumSize(QSize(1080, 740));
         gridLayout_2 = new QGridLayout(showCtrlBarBgWidget);
         gridLayout_2->setSpacing(0);
         gridLayout_2->setObjectName("gridLayout_2");
         gridLayout_2->setContentsMargins(0, 0, 0, 0);
-        ShowWidget = new QWidget(showCtrlBarBgWidget);
-        ShowWidget->setObjectName("ShowWidget");
-        ShowWidget->setMinimumSize(QSize(100, 100));
-
-        gridLayout_2->addWidget(ShowWidget, 0, 0, 1, 1);
-
         CtrlBarWidget = new CtrlBar(showCtrlBarBgWidget);
         CtrlBarWidget->setObjectName("CtrlBarWidget");
-        CtrlBarWidget->setMinimumSize(QSize(0, 60));
-        CtrlBarWidget->setMaximumSize(QSize(16777215, 60));
+        CtrlBarWidget->setMinimumSize(QSize(1080, 60));
+        CtrlBarWidget->setMaximumSize(QSize(1080, 60));
 
         gridLayout_2->addWidget(CtrlBarWidget, 1, 0, 1, 1);
+
+        ShowWidget = new DisplayBox(showCtrlBarBgWidget);
+        ShowWidget->setObjectName("ShowWidget");
+        ShowWidget->setMinimumSize(QSize(1080, 680));
+        ShowWidget->setMaximumSize(QSize(16777215, 16777215));
+
+        gridLayout_2->addWidget(ShowWidget, 0, 0, 1, 1);
 
 
         gridLayout->addWidget(showCtrlBarBgWidget, 0, 0, 1, 1);
@@ -75,26 +82,30 @@ public:
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menubar->setEnabled(true);
+        menubar->setGeometry(QRect(0, 0, 1281, 37));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
+        statusbar->setEnabled(true);
         MainWindow->setStatusBar(statusbar);
         PlaylistWidget = new QDockWidget(MainWindow);
         PlaylistWidget->setObjectName("PlaylistWidget");
-        PlaylistWidget->setMinimumSize(QSize(81, 39));
-        PlaylistContents = new QWidget();
+        PlaylistWidget->setMinimumSize(QSize(200, 762));
+        PlaylistContents = new PlayListBox();
         PlaylistContents->setObjectName("PlaylistContents");
+        PlaylistContents->setMinimumSize(QSize(200, 740));
         PlaylistWidget->setWidget(PlaylistContents);
-        MainWindow->addDockWidget(Qt::RightDockWidgetArea, PlaylistWidget);
+        MainWindow->addDockWidget(Qt::DockWidgetArea::RightDockWidgetArea, PlaylistWidget);
         TitleWidget_3 = new QDockWidget(MainWindow);
         TitleWidget_3->setObjectName("TitleWidget_3");
-        TitleWidget_3->setMinimumSize(QSize(81, 39));
-        TitleWidget_3->setFloating(true);
+        TitleWidget_3->setMinimumSize(QSize(1280, 82));
+        TitleWidget_3->setFloating(false);
         TitleContents_2 = new TitleBar();
         TitleContents_2->setObjectName("TitleContents_2");
+        TitleContents_2->setMinimumSize(QSize(1280, 60));
         TitleWidget_3->setWidget(TitleContents_2);
-        MainWindow->addDockWidget(Qt::TopDockWidgetArea, TitleWidget_3);
+        MainWindow->addDockWidget(Qt::DockWidgetArea::TopDockWidgetArea, TitleWidget_3);
 
         retranslateUi(MainWindow);
 
