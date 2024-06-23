d4116813966bcdb96898418591bb72d400686e69
fuck_if <wongjy13005151320@outlook.com>
2024-06-23 23:24:23 +0800

    Create playlistbox.cpp

---
+++ code/win/4-player/player/playlistbox.cpp
@@ -0,0 +1,18 @@
+//
+// Created by wong on 2024/6/22.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_PlayListBox.h" resolved
+
+#include "playlistbox.hpp"
+#include "ui_PlayListBox.h"
+
+
+PlayListBox::PlayListBox(QWidget *parent) :
+        QWidget(parent), ui(new Ui::PlayListBox) {
+    ui->setupUi(this);
+}
+
+PlayListBox::~PlayListBox() {
+    delete ui;
+}
