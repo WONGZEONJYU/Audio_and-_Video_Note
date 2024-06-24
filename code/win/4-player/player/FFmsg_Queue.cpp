e5a07c58e44b2498e7657ea84ee03105fb7213dc
fuck_if <wongjy13005151320@outlook.com>
2024-06-25 00:47:16 +0800

    new add

---
+++ code/win/4-player/player/FFmsg_Queue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wong on 2024/6/24.
+//
+
+#include "FFmsg_Queue.hpp"
---
+++ code/win/4-player/player/FFmsg_Queue.hpp
@@ -0,0 +1,26 @@
+//
+// Created by wong on 2024/6/24.
+//
+
+#ifndef PLAYER_FFMSG_QUEUE_HPP
+#define PLAYER_FFMSG_QUEUE_HPP
+
+#include <queue>
+#include <memory>
+#include <variant>
+
+struct AVMessage{
+    int what{};
+};
+
+class FFmsg_Queue {
+
+public:
+
+
+private:
+
+};
+
+
+#endif //PLAYER_FFMSG_QUEUE_HPP
