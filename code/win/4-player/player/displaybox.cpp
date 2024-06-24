59eac527ed108072d64976fbcef801c553e6ebf4
fuck_if <wongjy13005151320@outlook.com>
2024-06-25 00:47:20 +0800

    Update displaybox.cpp

--- code/win/4-player/player/displaybox.cpp
+++ code/win/4-player/player/displaybox.cpp
@@ -5,8 +5,7 @@
 // You may need to build the project (run Qt uic code generator) to get "ui_DisplayBox.h" resolved
 
 #include "displaybox.hpp"
-#include "ui_DisplayBox.h"
-
+#include "ui_displaybox.h"
 
 DisplayBox::DisplayBox(QWidget *parent) :
         QWidget(parent), ui(new Ui::DisplayBox) {
