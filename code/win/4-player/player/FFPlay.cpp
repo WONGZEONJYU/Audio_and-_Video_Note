63bcc7d32edd71d298db95814fdc9193a7ec81b0
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-06-27 18:02:38 +0800

    new add

---
+++ code/win/4-player/player/FFPlay.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2024/6/27.
+//
+
+#include "FFPlay.hpp"
---
+++ code/win/4-player/player/MessageAbstract.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2024/6/27.
+//
+
+#ifndef PLAYER_MESSAGEABSTRACT_HPP
+#define PLAYER_MESSAGEABSTRACT_HPP
+
+class MessageAbstract{
+
+public:
+    virtual void msg_loop() = 0;
+};
+
+#endif //PLAYER_MESSAGEABSTRACT_HPP
