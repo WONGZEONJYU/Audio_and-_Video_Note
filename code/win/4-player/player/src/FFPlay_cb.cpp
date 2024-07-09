78be3840ea849b3bcbaaa1eefccb94a2de8adc0f
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-07-09 18:01:04 +0800

    new add

---
+++ code/win/4-player/player/src/FFPlay_cb.cpp
@@ -0,0 +1,47 @@
+#include "FFPlay.hpp"
+#include <algorithm>
+
+int FFPlay::decode_interrupt_cb(void *_this)
+{
+    static int64_t s_pre_time {};
+    const auto cur_time  {av_gettime_relative() / 1000};
+    //std::cerr << "decode_interrupt_cb interval:\t" << (cur_time - s_pre_time) << "\n";
+    s_pre_time = cur_time;
+    auto this_{static_cast<FFPlay*>(_this)};
+    return this_->m_abort_request;
+}
+
+void FFPlay::sdl_audio_callback(void *_this, Uint8 *stream, int len)
+{
+    auto this_{static_cast<FFPlay*>(_this)};
+
+    while (len > 0){
+        if (this_->m_audio_buf_index >= this_->m_audio_buf_size) { //此处代表上一次剩余数据已经被拷贝完毕,需重获取新数据
+            const auto audio_size {this_->audio_decode_frame()};
+            if (audio_size < 0){
+                this_->m_audio_buf = nullptr;
+                this_->m_audio_buf_size = SDL_AUDIO_MIN_BUFFER_SIZE / this_->m_audio_tgt.frame_size * this_->m_audio_tgt.frame_size;
+
+            } else{
+                this_->m_audio_buf_size = audio_size; /*读到多少字节数据*/
+            }
+
+            this_->m_audio_buf_index = 0;
+        }
+
+        auto len1{this_->m_audio_buf_size - this_->m_audio_buf_index};
+
+        if (len1 > len){ //超出缓冲区给的长度,先拷贝缓冲区给出的长度
+            len1 = len;
+        }
+
+        if (this_->m_audio_buf){
+            std::copy_n(this_->m_audio_buf + this_->m_audio_buf_index,len1,stream);
+        }
+
+        len -= static_cast<int>(len1); //读取了多少数据,len要减去相应的长度
+        stream += len1; //stream也需偏移
+        /* 更新m_audio_buf_index,指向audio_buf中未被拷贝到stream的数据(剩余数据)的起始位置 */
+        this_->m_audio_buf_index += static_cast<int>(len1);
+    }
+}
---
+++ code/win/4-player/player/src/FFPlay_th.cpp
@@ -0,0 +1,111 @@
+#include "FFPlay.hpp"
+#include "ff_ffmsg.h"
+#include <iostream>
+#include <algorithm>
+
+#include "ShareAVPacket.hpp"
+#include "AVHelper.h"
+
+using namespace std;
+using namespace chrono;
+using namespace this_thread;
+
+void FFPlay::read_thread() {
+
+    cerr << __FUNCTION__ << "\tbegin\n";
+
+    ShareAVPacket_sp_type pkt;
+    int err,st_index[AVMEDIA_TYPE_NB]{};
+    std::fill_n(st_index,std::size(st_index),-1);
+
+    try {
+        pkt = new_ShareAVPacket(); //分配AVPacket
+
+        if (!(m_ic = avformat_alloc_context())){ //创建avformat上下文
+            throw std::runtime_error(string(__FUNCTION__) + "\tCould not allocate context.\n");
+        }
+
+        m_ic->interrupt_callback.callback = decode_interrupt_cb; //用于在读取媒体文件阻塞的时候,退出阻塞
+        m_ic->interrupt_callback.opaque = this;
+
+        if ((err = avformat_open_input(&m_ic,m_url.c_str(), nullptr, nullptr)) < 0){ //打开avformat
+            const auto errmsg(string (__FUNCTION__)+ "\t" + AVHelper::av_get_err(err));
+            throw std::runtime_error(errmsg);
+        }
+
+        mq_msg_put(FFP_MSG_OPEN_INPUT); //发送媒体文件打开消息
+        std::cerr << __FUNCTION__ << "\tFFP_MSG_OPEN_INPUT\n";
+
+        if ((err = avformat_find_stream_info(m_ic, nullptr)) < 0) { //有些流无法在avformat_open_input直接被识别,通过avformat_find_stream_info函数进行读包识别
+            const auto errmsg(string(__FUNCTION__ ) + "\t" + AVHelper::av_get_err(err));
+            throw std::runtime_error(errmsg);
+        }
+
+        mq_msg_put(FFP_MSG_FIND_STREAM_INFO); //发送寻媒体流消息
+        std::cerr << __FUNCTION__ << "\tFFP_MSG_COMPONENT_OPEN\n";
+
+        st_index[AVMEDIA_TYPE_AUDIO] = av_find_best_stream(m_ic, AVMEDIA_TYPE_AUDIO,
+                                                           st_index[AVMEDIA_TYPE_AUDIO],-1,nullptr,0);
+        //读取音频流index
+
+        st_index[AVMEDIA_TYPE_VIDEO] = av_find_best_stream(m_ic, AVMEDIA_TYPE_VIDEO,
+                                                           st_index[AVMEDIA_TYPE_VIDEO],-1,nullptr,0);
+        //读取视频流index
+
+        if (st_index[AVMEDIA_TYPE_VIDEO] >= 0){ //打开视频流
+            stream_component_open(st_index[AVMEDIA_TYPE_VIDEO]);
+        }
+
+        if (st_index[AVMEDIA_TYPE_AUDIO] >= 0){ //打开音频流
+            stream_component_open(st_index[AVMEDIA_TYPE_AUDIO]);
+        }
+
+        mq_msg_put(FFP_MSG_COMPONENT_OPEN); //发送打开媒体流消息
+
+        if (m_video_stream < 0 && m_audio_stream < 0){ //音视频流都打开失败
+            throw std::runtime_error("Failed to open file\n");
+        }
+
+        mq_msg_put(FFP_MSG_PREPARED);
+        std::cerr << __FUNCTION__ << "\tFFP_MSG_PREPARED\n";
+
+        //int ret;
+        while (!m_abort_request){
+            auto ret{av_read_frame(m_ic, *pkt)}; //不会释放pkt的数据,需要我们自己释放packet的数据
+            if (ret < 0){ //出错或者读取完毕
+                if (AVERROR_EOF == ret || avio_feof(m_ic->pb) && !m_eof){
+                    m_eof = true;
+                }
+                if (m_ic->pb && m_ic->pb->error){ //io异常
+                    break;
+                }
+                sleep_for(10ms); //休眠以下,主要
+                continue;
+            } else{
+                m_eof = false;
+            }
+
+            if (m_audio_stream == (*pkt)->stream_index) {
+                packet_queue_put(&m_audioq,*pkt); //音频插入队列
+            } else{
+                av_packet_unref(*pkt); //不入队则直接释放数据
+            }
+        }
+
+    } catch (const std::exception &e) {
+        pkt.reset();
+        avformat_close_input(&m_ic);
+        mq_msg_put(FFP_MSG_ERROR); //线程内出错,通知UI和IJKMediaPlay处理事件
+        cerr << e.what() << "\n";
+    }
+
+    cerr << __FUNCTION__  << "\tend\n";
+}
+
+void FFPlay::video_refresh_thread() {
+    cerr << __FUNCTION__ << "\tbegin\n";
+    while (!m_abort_request){
+        sleep_for(10ms);
+    }
+    cerr << __FUNCTION__  << "\tend\n" <<flush;
+}
