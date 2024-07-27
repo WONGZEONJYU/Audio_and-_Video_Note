268df1b91f5702d3f82f39e00b17c3e480a38959
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-07-27 10:16:00 +0800

    m

--- code/win/4-player/TestQtOpenGL/XVideoWidget.cpp
+++ code/win/4-player/TestQtOpenGL/XVideoWidget.cpp
@@ -17,53 +17,14 @@ static inline constexpr int T_VER{1};
 //attribute
 //varying
 
-//顶点shader
-static inline constexpr auto vString{
-R"glsl(
-    #version 410
-    in vec4 vertexIn;
-    in vec2 textureIn;
-    out vec2 textureOut;
-    void main(void){
-        gl_Position = vertexIn;
-        textureOut = textureIn;
-    }
-)glsl"
-};
 
-//gl_FragColor
-static inline constexpr auto tString{
-R"glsl(
-    #version 410
-    in vec2 textureOut;
-    uniform sampler2D tex_y;
-    uniform sampler2D tex_u;
-    uniform sampler2D tex_v;
-    out vec4 FragColor;
-    void main(void){
-        vec3 yuv;
-        vec3 rgb;
-        yuv.x = texture(tex_y, textureOut).r;
-        yuv.y = texture(tex_u, textureOut).r - 0.5;
-        yuv.z = texture(tex_v, textureOut).r - 0.5;
-        rgb = mat3(1.0, 1.0, 1.0,
-                   0.0, -0.39465, 2.03211,
-                   1.13983, -0.58060, 0.0) * yuv;
-        FragColor = vec4(rgb, 1.0);
-    }
-)glsl"
-};
 
 XVideoWidget::XVideoWidget(QWidget *parent):QOpenGLWidget(parent){
 
 }
 
 XVideoWidget::~XVideoWidget() {
-    for (auto & i:m_datas) {
-        delete [] i;
-        i = nullptr;
-    }
-    std::fill_n(m_datas,std::size(m_datas), nullptr);
+
 }
 
 //初始化opengl
@@ -85,12 +46,6 @@ void XVideoWidget::initializeGL() {
     qDebug() << m_program.addShaderFromSourceCode(QOpenGLShader::Vertex,vString);
     qDebug() << m_program.log();
 
-    //设置顶点坐标的变量
-    m_program.bindAttributeLocation(GET_STR(vertexIn),A_VER);
-
-    //设置材质坐标
-    m_program.bindAttributeLocation(GET_STR(textureIn),T_VER);
-
     //编译shader
     qDebug() << "program.link() = " << m_program.link();
     qDebug() << m_program.log();
@@ -99,17 +54,27 @@ void XVideoWidget::initializeGL() {
     qDebug() << m_program.log();
 
     GL_CHECK(glUseProgram(m_program.programId()));
+
+    glGenBuffers(1,&m_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER,m_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(ver), ver, GL_STATIC_DRAW);
+
+    glGenVertexArrays(1,&m_vao);
+    glBindVertexArray(m_vao);
+
     //传递顶点和材质坐标
     const auto vertexIn_num {m_program.attributeLocation(GET_STR(vertexIn))};
     qDebug() << "vertexIn_num = " << vertexIn_num;
     //顶点
-    GL_CHECK(glVertexAttribPointer(vertexIn_num, 2, GL_FLOAT, GL_FALSE, 0, ver));
+    GL_CHECK(glVertexAttribPointer(vertexIn_num, 2, GL_FLOAT, GL_FALSE,
+                                   4 * sizeof(float ), nullptr));
     GL_CHECK(glEnableVertexAttribArray(vertexIn_num));
 
     const auto textureIn_num {m_program.attributeLocation(GET_STR(textureIn))};
     qDebug() << "textureIn_num = " << textureIn_num;
     //材质
-    GL_CHECK(glVertexAttribPointer(textureIn_num, 2, GL_FLOAT, GL_FALSE, 0, tex));
+    GL_CHECK(glVertexAttribPointer(textureIn_num, 2, GL_FLOAT, GL_FALSE,
+                                   4 * sizeof(float ), reinterpret_cast<void *>(2 * sizeof (float ))));
     GL_CHECK(glEnableVertexAttribArray(textureIn_num));
 
     //从shader获取材质
@@ -150,10 +115,11 @@ void XVideoWidget::initializeGL() {
                  0,GL_RED,GL_UNSIGNED_BYTE, nullptr));
     /*****************************************V******************************************/
 
-    //分配材质内存空间
-    m_datas[0] = new uint8_t[m_w * m_h]{};      //Y
-    m_datas[1] = new uint8_t[m_w * m_h / 4]{}; //U
-    m_datas[2] = new uint8_t[m_w * m_h / 4]{}; //V
+    GL_CHECK(glUniform1i(m_unis[0], 0));
+    GL_CHECK(glUniform1i(m_unis[1], 1));
+    GL_CHECK(glUniform1i(m_unis[2], 2));
+
+    glBindVertexArray(0);
 
     m_file.setFileName(GET_STR(out240x128.yuv));
     if (!m_file.open(QFile::ReadOnly)){
@@ -172,45 +138,46 @@ void XVideoWidget::paintGL() {
 
     qDebug() << "begin: " << __FUNCTION__;
 
+    glBindVertexArray(m_vao);
+
     if (m_file.atEnd()){
         m_file.seek(0);
     }
-
-    m_file.read(reinterpret_cast<char *>(m_datas[0]),m_w * m_h);
-    m_file.read(reinterpret_cast<char *>(m_datas[1]),m_w * m_h / 4);
-    m_file.read(reinterpret_cast<char *>(m_datas[2]),m_w * m_h / 4);
-
+    auto data{m_file.read(m_w*m_h)};
     /****************************************y****************************************/
     GL_CHECK(glActiveTexture(GL_TEXTURE0));//激活了0层材质
     GL_CHECK(glBindTexture(GL_TEXTURE_2D,m_texs[0])); //0层绑定到Y材质
     //修改材质内容(复印内存中内容)
     GL_CHECK(glTexSubImage2D(GL_TEXTURE_2D,0,0,0,m_w,m_h,
-                    GL_RED,GL_UNSIGNED_BYTE,m_datas[0]));
+                    GL_RED,GL_UNSIGNED_BYTE,data));
     //与shader uni变量关联
-    GL_CHECK(glUniform1i(m_unis[0], 0));
+    //GL_CHECK(glUniform1i(m_unis[0], 0));
     /****************************************y****************************************/
-
+    data = std::move(m_file.read(m_w*m_h/4));
     /****************************************u****************************************/
     GL_CHECK(glActiveTexture(GL_TEXTURE0 + 1));//激活了1层材质
     GL_CHECK(glBindTexture(GL_TEXTURE_2D,m_texs[1])); //1层绑定到U材质
     //修改材质内容(复印内存中内容)
     GL_CHECK(glTexSubImage2D(GL_TEXTURE_2D,0,0,0,m_w / 2,m_h / 2,
-                    GL_RED,GL_UNSIGNED_BYTE,m_datas[1]));
+                    GL_RED,GL_UNSIGNED_BYTE,data));
     //与shader uni变量关联
-    GL_CHECK(glUniform1i(m_unis[1], 1));
+    //GL_CHECK(glUniform1i(m_unis[1], 1));
     /****************************************u****************************************/
 
+    data = std::move(m_file.read(m_w*m_h/4));
     /****************************************v****************************************/
     GL_CHECK(glActiveTexture(GL_TEXTURE0 + 2));//激活了2层材质
     GL_CHECK(glBindTexture(GL_TEXTURE_2D,m_texs[2])); //2层绑定到V材质
     //修改材质内容(复印内存中内容)
     GL_CHECK(glTexSubImage2D(GL_TEXTURE_2D,0,0,0,m_w / 2,m_h / 2,
-                    GL_RED,GL_UNSIGNED_BYTE,m_datas[2]));
+                    GL_RED,GL_UNSIGNED_BYTE,data));
     //与shader uni变量关联
-    GL_CHECK(glUniform1i(m_unis[2], 2));
+    //GL_CHECK(glUniform1i(m_unis[2], 2));
     /****************************************v****************************************/
 
     GL_CHECK(glDrawArrays(GL_TRIANGLE_STRIP,0,4));
+
+    glBindVertexArray(0);
 
     qDebug() << "end: " << __FUNCTION__;
 }
--- code/win/4-player/TestQtOpenGL/XVideoWidget.hpp
+++ code/win/4-player/TestQtOpenGL/XVideoWidget.hpp
@@ -15,34 +15,64 @@
 #include <QOpenGLWidget>
 #include <QTimer>
 #include <QOpenGLFunctions>
+#include <QOpenGLExtraFunctions>
 
-class XVideoWidget final : public QOpenGLWidget,protected QOpenGLFunctions
+class XVideoWidget final : public QOpenGLWidget,
+        protected QOpenGLExtraFunctions
 {
+    //顶点shader
+    static inline constexpr auto vString{
+            R"glsl(
+    #version 410
+    layout(location = 0)in vec2 vertexIn;
+    layout(location = 1)in vec2 textureIn;
+    out vec2 textureOut;
+    void main(void){
+        gl_Position = vec4(vertexIn,0.0,1.0);
+        textureOut = textureIn;
+    }
+)glsl"
+};
+
+//gl_FragColor
+//片元shader
+    static inline constexpr auto tString{
+            R"glsl(
+    #version 410
+    in vec2 textureOut;
+    uniform sampler2D tex_y;
+    uniform sampler2D tex_u;
+    uniform sampler2D tex_v;
+    out vec4 FragColor;
+    void main(void){
+        vec3 yuv;
+        vec3 rgb;
+        yuv.x = texture(tex_y, textureOut).r;
+        yuv.y = texture(tex_u, textureOut).r - 0.5;
+        yuv.z = texture(tex_v, textureOut).r - 0.5;
+        rgb = mat3(1.0, 1.0, 1.0,
+                   0.0, -0.39465, 2.03211,
+                   1.13983, -0.58060, 0.0) * yuv;
+        FragColor = vec4(rgb, 1.0);
+    }
+)glsl"
+};
+
     //顶点坐标
     static inline constexpr GLfloat ver[] {
-//逆时针,
-            -1.0f,-1.0f,
-            1.0f,-1.0f,
-            -1.0f,1.0f,
-            1.0f,1.0f,
-//顺时针
-//        1.0f,-1.0f,0.0f,
-//        -1.0f,-1.0,0.0f,
-//        1.0f,1.0f,0.0f,
-//        -1.0f,1.0f,0.0f
+            //顶点坐标                  //纹理坐标
+            -1.0f,-1.0f,  0.0f, 1.0f,
+            1.0f,-1.0f, 1.0f, 1.0f,
+            -1.0f,1.0f, 0.0f, 0.0f,
+            1.0f,1.0f, 1.0f, 0.0f,
     };
 
     static inline constexpr GLfloat tex[] {
-//逆时针
+
             0.0f, 1.0f,
             1.0f, 1.0f,
             0.0f, 0.0f,
             1.0f, 0.0f,
-//顺时针
-//            1.0f,0.0f,
-//            0.0f,0.0f,
-//            1.0f,1.0f,
-//            0.0f,1.0f
     };
 
 Q_OBJECT
@@ -57,6 +87,7 @@ public:
 private:
     //shader程序
     QOpenGLShaderProgram m_program;
+    uint32_t m_vao{},m_vbo{},m_ebo{};
 
     //shader中yuv变量地址
     GLint m_unis[3]{};
@@ -64,7 +95,7 @@ private:
     GLuint m_texs[3]{};
 
     //材质内存空间
-    uint8_t *m_datas[3]{};
+    //uint8_t *m_datas[3]{};
 
     int m_w{240},m_h{128};
 
