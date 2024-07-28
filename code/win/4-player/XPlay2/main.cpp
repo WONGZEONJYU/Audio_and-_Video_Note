654af2d9f4a89a14d3d57f514c2d3f9d77d12736
fuck_if <wongjy13005151320@outlook.com>
2024-07-29 00:10:50 +0800

    Update main.cpp

--- code/win/4-player/XPlay2/main.cpp
+++ code/win/4-player/XPlay2/main.cpp
@@ -1,10 +1,20 @@
 #include <QApplication>
+#include <QSurfaceFormat>
 #include "XPlay2Widget.hpp"
+#include "XDemux.hpp"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    try {
 
+        XDemux dmux,d1;
+        dmux.Open("2_audio_track_5s.mp4");
+        d1.Open("2_audio_track_5s.mp4");
 
-
-    return QApplication::exec();
+        XPlay2Widget::Handle()->show();
+        return QApplication::exec();
+    } catch (const std::exception &e) {
+        qDebug() << e.what();
+        return -1;
+    }
 }
