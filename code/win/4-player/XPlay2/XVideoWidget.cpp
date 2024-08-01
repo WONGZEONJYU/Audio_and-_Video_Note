2f4b41d81686c76e4cbaf036469990d3dfea384f
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-08-01 18:48:49 +0800

    new add

---
+++ code/win/4-player/XPlay2/XVideoWidget.cpp
@@ -0,0 +1,261 @@
+//
+// Created by Administrator on 2024/8/1.
+//
+
+#include "XVideoWidget.hpp"
+#include <QByteArray>
+#include "XAVFrame.hpp"
+
+XVideoWidget::XVideoWidget(QWidget *parent):QOpenGLWidget(parent){
+}
+
+XVideoWidget::~XVideoWidget() {
+    cleanup();
+}
+
+//初始化opengl
+void XVideoWidget::initializeGL() {
+
+    qDebug() << "begin " << __FUNCTION__ ;
+
+    //初始化opengl
+    initializeOpenGLFunctions();
+
+    qDebug() << "OpenGL version: " << reinterpret_cast<const char*>(glGetString(GL_VERSION));
+    qDebug() << "OpenGL GLSL version: " <<  reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    //program加载shader(顶点和片元)脚本
+    //m_shader = new QOpenGLShaderProgram(this);
+    m_shader.reset(new QOpenGLShaderProgram(this));
+
+    //m_shader->create(); //通常不需要手动调用，因为 addShader, link, 等函数会隐式创建。
+    //顶点shader
+    qDebug() << m_shader->addShaderFromSourceCode(QOpenGLShader::Vertex,Vertex_shader);
+    qDebug() << m_shader->log();
+
+    //片元shader
+    qDebug() << m_shader->addShaderFromSourceCode(QOpenGLShader::Fragment,Fragment_shader);
+    qDebug() << m_shader->log();
+
+    //编译shader
+    qDebug() << "m_shader->link() = " << m_shader->link();
+    qDebug() << m_shader->log();
+    //绑定shader
+    qDebug() << "program.bind() = " << m_shader->bind();
+    qDebug() << m_shader->log();
+
+    // VAO VBO EBO详情看链接到解释
+    // https://blog.csdn.net/weixin_44179561/article/details/124275761
+    //https://learnopengl-cn.readthedocs.io/zh/latest/01%20Getting%20started/05%20Shaders/
+    m_VAO.reset(new QOpenGLVertexArrayObject(this));
+//    m_VAO->create();
+//    m_VAO->bind();
+    QOpenGLVertexArrayObject::Binder vao(m_VAO.get());//代替m_VAO->create(); m_VAO->bind();m_VAO->release();
+    //采用RAII技术
+
+    m_VBO.reset(new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer));
+    m_VBO->create();
+    qDebug() << "m_VBO->bind() = " << m_VBO->bind();
+    m_VBO->allocate(ver_tex_coordinate, sizeof(ver_tex_coordinate));
+    m_VBO->setUsagePattern(QOpenGLBuffer::StaticDraw);
+
+    m_EBO.reset(new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer));
+    m_EBO->create();
+    qDebug() << "m_EBO->bind() = " << m_EBO->bind();
+    m_EBO->allocate(direction, sizeof(direction));
+
+    //传递顶点和材质坐标
+    const auto vertexIn_num {m_shader->attributeLocation(GET_STR(vertexIn))};
+    qDebug() << "vertexIn_num = " << vertexIn_num;
+    //顶点坐标设置
+    m_shader->setAttributeArray(vertexIn_num, //顶点着色器vertexIn的location编号
+                                GL_FLOAT, //坐标类型
+                                nullptr, //坐标数组的起始位置
+                                2, //坐标数量,例如(x,y)
+                                4* sizeof(float )); //访问偏移
+    m_shader->enableAttributeArray(vertexIn_num); //启用顶点坐标
+
+//    GL_CHECK(glVertexAttribPointer(vertexIn_num, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float ), nullptr));
+//    GL_CHECK(glEnableVertexAttribArray(vertexIn_num));
+
+    const auto textureIn_num {m_shader->attributeLocation(GET_STR(textureIn))};
+    qDebug() << "textureIn_num = " << textureIn_num;
+
+    //纹理(材质)坐标设置
+    m_shader->setAttributeArray(textureIn_num, //顶点着色器纹理textureIn的location编号
+                                GL_FLOAT,//同上
+                                reinterpret_cast<const void *>(2 * sizeof(float)), //坐标数组的起始位置
+                                2,//同上
+                                4 * sizeof(float));//同上
+    m_shader->enableAttributeArray(textureIn_num); //同上
+
+//    GL_CHECK(glVertexAttribPointer(textureIn_num, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+//                     reinterpret_cast<const void *>(2 * sizeof(float))));
+//    GL_CHECK(glEnableVertexAttribArray(textureIn_num));
+
+    //关联片元着色器的uniform sampler2D(均匀采样器2D)
+    m_shader->setUniformValue("tex_y",0);
+    m_shader->setUniformValue("tex_u",1);
+    m_shader->setUniformValue("tex_v",2);
+
+    //m_VAO->release();//被QOpenGLVertexArrayObject::Binder vao(m_VAO);取代
+    m_EBO->release();
+    m_VBO->release();
+    m_shader->release();
+
+    qDebug() << "end " << __FUNCTION__ ;
+}
+
+void XVideoWidget::paintGL() {
+
+    qDebug() << "begin: " << __FUNCTION__;
+
+    m_shader->bind();
+    QOpenGLVertexArrayObject::Binder vao(m_VAO.get());
+    //m_VAO->bind(); //被QOpenGLVertexArrayObject::Binder vao(m_VAO);取代
+    m_VBO->bind();
+    m_EBO->bind();
+
+    GL_CHECK(glClear(GL_COLOR_BUFFER_BIT));
+    GL_CHECK(glPixelStorei(GL_UNPACK_ALIGNMENT, 1));//用于支持竖屏
+
+//    for (int i {}; i < std::size(m_textureYUV); ++i) {
+//        //Y数据的长度m_w * m_h
+//        //U数据的长度 m_w * m_h / 4
+//        //V数据的长度 m_w * m_h / 4
+//        const auto len{i ? m_w * m_h / 4 : m_w * m_h};
+//        m_textureYUV[i]->bind(i);
+//        m_textureYUV[i]->setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8, nullptr);
+//    }
+
+    for(int i{};auto &item:m_textureYUV) {
+        //Y数据的长度m_w * m_h
+        //U数据的长度 m_w * m_h / 4
+        //V数据的长度 m_w * m_h / 4
+        item->bind(i);
+        item->setData(QOpenGLTexture::Red, QOpenGLTexture::UInt8,
+                      reinterpret_cast<const uint8_t *>(m_yuv_datum[i].data()));
+        ++i;
+    }
+
+    GL_CHECK(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+    //GL_CHECK(glDrawArrays(GL_TRIANGLE_STRIP, 0, 4));
+
+    for (auto &item:m_textureYUV) {
+        item->release();
+    }
+
+    m_shader->release();
+    //m_VAO->release(); //被QOpenGLVertexArrayObject::Binder vao(m_VAO);取代
+    m_VBO->release();
+    m_EBO->release();
+
+    qDebug() << "end: " << __FUNCTION__;
+}
+
+void XVideoWidget::resizeGL(int w, int h) {
+    qDebug() << __FUNCTION__ << " w:" << w << " h:" << h;
+    GL_CHECK(glViewport(0, 0, w, h));
+}
+
+void XVideoWidget::cleanup() noexcept(true) {
+
+    makeCurrent();
+
+    m_yuv_datum.clear();
+
+    for(auto &item:m_textureYUV){
+        if (item){
+            item->release();
+            item->destroy();
+        }
+    }
+    m_textureYUV.clear();
+
+    if (m_shader){
+        m_shader->release();
+    }
+    m_shader.reset();
+
+    if (m_VAO){
+        m_VAO->release();
+        m_VAO->destroy();
+    }
+    m_VAO.reset();
+
+    if (m_VBO){
+        m_VBO->release();
+        m_VBO->destroy();
+    }
+    m_VBO.reset();
+
+    if (m_EBO){
+        m_EBO->release();
+        m_EBO->destroy();
+    }
+    m_EBO.reset();
+
+    doneCurrent();
+}
+
+void XVideoWidget::Init(const int &w,const int&h) noexcept(false){
+
+    QMutexLocker locker(&m_mux);
+    m_w = w,m_h = h;
+
+    /*分配纹理(材质)内存空间,并设置参数,并分配显存空间*/
+    try {
+        m_yuv_datum.clear();
+        m_yuv_datum.resize(3);
+
+        m_textureYUV.clear();
+        m_textureYUV.resize(3);
+        for(int i {};auto &item : m_textureYUV){
+            CHECK_EXC(item.reset(new QOpenGLTexture(QOpenGLTexture::Target2D)));
+
+            const auto _w{i ? w / 2 : w},_h {i ? h / 2 : h};
+            item->setSize(_w,_h);
+
+            item->setFormat(QOpenGLTexture::R8_UNorm);
+            /*
+                R8：表示纹理中的每个像素都有一个 8 位的红色通道。没有绿色、蓝色或 alpha 通道。
+                UNorm：表示无符号归一化（Unsigned Normalized），
+                归一化的意思是纹理数据会映射到 [0.0, 1.0] 范围，
+                对于 8 位无符号整数，0 映射到 0.0，255 映射到 1.0。
+            */
+            item->setMinMagFilters(QOpenGLTexture::Linear, QOpenGLTexture::Linear);
+            /*放大过滤,线性插值*/
+            item->setWrapMode(QOpenGLTexture::ClampToEdge);
+            /*表示当纹理坐标超出[0.0, 1.0] 范围时,将纹理坐标钳制到边缘值*/
+            item->allocateStorage();
+            /*分配显存*/
+            qDebug() << "textureId: "  << item->textureId();
+            ++i;
+        }
+    } catch (...) {
+        cleanup();
+        locker.unlock();
+        std::rethrow_exception(std::current_exception());
+    }
+}
+
+void XVideoWidget::Repaint(const XAVFrame_sptr &frame) {
+
+    if (!frame){
+        qDebug() << "\n";
+        return;
+    }
+
+    {
+        QMutexLocker locker(&m_mux);
+        for (uint32_t i{};auto &item:m_yuv_datum) {
+
+            const auto len{ i ? m_w * m_h / 4 : m_w * m_h};
+            //item = {reinterpret_cast<const char*>(frame->data[i]),len};
+            item = std::move(QByteArray(reinterpret_cast<const char*>(frame->data[i]),len));
+            ++i;
+        }
+    }
+
+    update();
+}
---
+++ code/win/4-player/XPlay2/XVideoWidget.hpp
@@ -0,0 +1,147 @@
+//
+// Created by Administrator on 2024/8/1.
+//
+
+#ifndef XPLAY2_XVIDEOWIDGET_HPP
+#define XPLAY2_XVIDEOWIDGET_HPP
+
+#if defined(__APPLE__) && defined(__MACH__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#endif
+
+#include <QOpenGLShaderProgram>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLTexture>
+#include <QMutex>
+#include <QSharedPointer>
+#include <QVector>
+#include <QScopedArrayPointer>
+#include <QByteArray>
+#include "XHelper.hpp"
+
+class XAVFrame;
+using XAVFrame_sptr = std::shared_ptr<XAVFrame>;
+
+class XVideoWidget final : public QOpenGLWidget,
+                           protected QOpenGLFunctions
+{
+//attribute新版已经弃用
+//varying新版已经弃用
+//一律用in/out代替
+
+//顶点shader
+    static inline constexpr auto Vertex_shader{
+            R"glsl(
+        #version 410 core
+        layout(location = 0) in vec2 vertexIn;
+        layout(location = 1) in vec2 textureIn;
+        out vec2 textureOut;
+        void main(){
+            gl_Position = vec4(vertexIn,0.0,1.0);
+            textureOut = textureIn;
+        }
+        )glsl"
+    };
+
+#if 1
+//gl_FragColor新版已经弃用
+    static inline constexpr auto Fragment_shader{
+            R"glsl(
+        #version 410 core
+        out vec4 FragColor;
+        in vec2 textureOut;
+        uniform sampler2D tex_y;
+        uniform sampler2D tex_u;
+        uniform sampler2D tex_v;
+        void main(){
+            vec3 yuv;
+            vec3 rgb;
+            yuv.x = texture(tex_y, textureOut).r;
+            yuv.y = texture(tex_u, textureOut).r - 0.5;
+            yuv.z = texture(tex_v, textureOut).r - 0.5;
+            rgb = mat3(1.0, 1.0, 1.0,
+                       0.0, -0.39465, 2.03211,
+                       1.13983, -0.58060, 0.0) * yuv;
+            FragColor = vec4(rgb, 1.0);
+        }
+        )glsl"
+    };
+#else
+    static inline constexpr auto tString{
+        R"glsl(
+        #version 410 core
+        out vec4 FragColor;
+        in vec2 textureOut;
+        uniform sampler2D tex_y;
+        uniform sampler2D tex_u;
+        uniform sampler2D tex_v;
+        void main(){
+            float y = texture(tex_y, textureOut).r;
+            float u = texture(tex_u, textureOut).r - 0.5;
+            float v = texture(tex_v, textureOut).r - 0.5;
+            float r = y + 1.13983 * v;
+            float g = y - 0.39465 * u - 0.5806 * v;
+            float b = y + 2.03211 * u;
+            FragColor = vec4(r,g,b,1.0);
+        }
+        )glsl"
+    };
+#endif
+
+    //顶点坐标
+    static inline constexpr GLfloat ver_tex_coordinate[] {
+            //顶点坐标       //纹理(材质)坐标
+            -1.0f,-1.0f,    0.0f, 1.0f,
+            1.0f,-1.0f,     1.0f, 1.0f,
+            -1.0f,1.0f,     0.0f, 0.0f,
+            1.0f,1.0f,      1.0f, 0.0f,
+    };
+
+    //绘制方向
+    static inline constexpr GLuint direction[]{
+            0,1,2,
+            2,3,1
+    };
+
+Q_OBJECT
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int , int ) override;
+    void cleanup() noexcept(true);
+
+public:
+    explicit XVideoWidget(QWidget*  = nullptr);
+    ~XVideoWidget() override;
+    void Init(const int &w,const int&h) noexcept(false);
+    void Repaint(const XAVFrame_sptr &);
+
+private:
+    QMutex m_mux;
+
+    //shader程序
+    //QOpenGLShaderProgram *m_shader{};
+    QSharedPointer<QOpenGLShaderProgram> m_shader;
+
+    //显存空间,用于存储顶点坐标
+    //QOpenGLBuffer *m_VBO{},*m_EBO{};
+    QSharedPointer<QOpenGLBuffer> m_VBO,m_EBO;
+
+    //顶点内存对象
+    //QOpenGLVertexArrayObject *m_VAO{};
+    QSharedPointer<QOpenGLVertexArrayObject> m_VAO;
+
+    //材质内存空间,显卡的材质空间
+    //QOpenGLTexture *m_textureYUV[3]{};
+    QVector<QSharedPointer<QOpenGLTexture>> m_textureYUV;
+
+    //YUV数据空间
+    QVector<QByteArray> m_yuv_datum;
+
+    int m_w{},m_h{};
+};
+
+#endif
