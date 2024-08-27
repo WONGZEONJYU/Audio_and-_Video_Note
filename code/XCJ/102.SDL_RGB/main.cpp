069cc3e5778641d3bbace430fd61b58f5fc6faa2
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-08-27 17:57:43 +0800

    Update main.cpp

--- code/XCJ/102.SDL_RGB/main.cpp
+++ code/XCJ/102.SDL_RGB/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <thread>
 #include <SDL.h>
 
 #undef main
@@ -11,58 +10,110 @@ int main(const int argc,const char **argv) {
 
     SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
 
-    auto Window{SDL_CreateWindow("102.SDL_RGB", 0, 0, Width, Height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)};
+    auto Window{SDL_CreateWindow("102.SDL_RGB",
+                                 SDL_WINDOWPOS_CENTERED,
+                                 SDL_WINDOWPOS_CENTERED,
+                                 Width,
+                                 Height,
+                                 SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE)};
+    /*创建窗口*/
     if (!Window) {
         std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
         return -1;
     }
 
-    auto Renderer{SDL_CreateRenderer(Window,-1, SDL_RENDERER_ACCELERATED)}; //创建渲染器
+    auto Renderer{SDL_CreateRenderer(Window,
+                                     -1,
+                                     SDL_RENDERER_ACCELERATED)};
+    /*创建渲染器*/
     if (!Renderer) {
+        SDL_DestroyWindow(Window);
         std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
         return -1;
     }
 
-    auto Texture{SDL_CreateTexture(Renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STREAMING,Width,Height)};
-    if (!Texture){
+    auto Texture{SDL_CreateTexture(Renderer,
+                                   SDL_PIXELFORMAT_ARGB8888,
+                                   SDL_TEXTUREACCESS_STREAMING,
+                                   Width,
+                                   Height)};
+    /*创建纹理*/
+    if (!Texture) {
+        SDL_DestroyRenderer(Renderer);
+        SDL_DestroyWindow(Window);
         std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
         return -1;
     }
 
-    if (SDL_RenderClear(Renderer) < 0){
-        std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
-        return -1;
-    }
+    std::vector<uint8_t> v(Width * Height * 4);
+    uint32_t rgb[3]{0xff};
+    int i{},count{255};
+    SDL_Rect rect{0,0,Width,Height};
 
-    std::vector<uint8_t> v(Width * 4 * Height);
+    while (true) {
 
-    const auto dst_{v.data()};
-
-    for (uint32_t h {}; h < Height; ++h) {
-        const auto drift{ h * Width * 4 };
-        for (uint32_t w {}; w < Width * 4; w += 4) {
-            dst_[drift + w] = 0xff;
-            dst_[drift + w + 1] = 0;
-            dst_[drift + w + 2] = 0;
-            dst_[drift + w + 3] = 0x7f;
+        SDL_Event sdlEvent{};
+        if (SDL_WaitEventTimeout(&sdlEvent,10) < 0){ //SDL事件
+            SDL_DestroyRenderer(Renderer);
+            SDL_DestroyWindow(Window);
+            SDL_DestroyTexture(Texture);
+            return -1;
+        }else {
+            if (sdlEvent.type == SDL_QUIT) {
+                break;
+            }
         }
+
+        if (SDL_RenderClear(Renderer) < 0) { //清除渲染器
+            std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
+            SDL_DestroyRenderer(Renderer);
+            SDL_DestroyWindow(Window);
+            SDL_DestroyTexture(Texture);
+            return -1;
+        }
+
+        if (!(--count)) {
+            count = 255;
+            rgb[i++] = 0;
+            i %= std::size(rgb);
+            rgb[i] = 0xff;
+        }
+
+        --rgb[i];
+
+        v.clear();
+        const auto dst_{v.data()};
+        for (uint32_t h {}; h < Height; ++h) {
+            const auto drift{ h * Width * 4 };
+            for (uint32_t w {}; w < Width * 4; w += 4) {
+                dst_[drift + w] = rgb[2];       //B
+                dst_[drift + w + 1] = rgb[1];   //G
+                dst_[drift + w + 2] = rgb[0];   //R
+                dst_[drift + w + 3] = 0;        //A
+            }
+        }
+
+        if (SDL_UpdateTexture(Texture, nullptr, dst_ ,Width * 4) < 0) { //更新纹理
+            std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
+            SDL_DestroyRenderer(Renderer);
+            SDL_DestroyWindow(Window);
+            SDL_DestroyTexture(Texture);
+            return -1;
+        }
+
+        if (SDL_RenderCopy(Renderer,Texture, nullptr, &rect) < 0) { //纹理数据拷贝到渲染器
+            std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
+            SDL_DestroyRenderer(Renderer);
+            SDL_DestroyWindow(Window);
+            SDL_DestroyTexture(Texture);
+            return -1;
+        }
+
+        SDL_RenderPresent(Renderer); //开始渲染
     }
-
-    if (SDL_UpdateTexture(Texture, nullptr,dst_,Width * 4 ) < 0){
-        std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    if(SDL_RenderCopy(Renderer,Texture, nullptr, nullptr) < 0){
-        std::cerr << __LINE__ << ": " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    SDL_RenderPresent(Renderer);
-
-    getchar();
 
     SDL_DestroyRenderer(Renderer);
-
+    SDL_DestroyWindow(Window);
+    SDL_DestroyTexture(Texture);
     return 0;
 }
