b4bbce901effb720a246e1a217b5a3fb206cfa21
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-09-04 09:56:50 +0800

    Update xsdl.cpp

--- code/XCJ/112.yuv_rgb_player/xsdl.cpp
+++ code/XCJ/112.yuv_rgb_player/xsdl.cpp
@@ -92,7 +92,6 @@ bool XSDL::Init(const int &w,const int &h,const Format &fmt) {
                                                    pix_fmt,
                                                    SDL_TEXTUREACCESS_STREAMING,
                                                    m_width,m_height),return {});
-
     return true;
 }
 
@@ -208,9 +207,6 @@ bool XSDL::Start_Rendering() {
         rect.h = m_scale_h;
         p_rect = std::addressof(rect);
     }
-
-    //const SDL_Rect rect{.x = m_x,.y = m_y,.w = m_scale_w,.h = m_scale_h};
-    //const SDL_Rect *p_rect{std::addressof(rect)};
 
     SDL2_INT_ERR_OUT(SDL_RenderCopyEx(m_renderer,m_texture,{},p_rect,{},{},SDL_FLIP_NONE),return {});
     //拷贝纹理数据到渲染器
