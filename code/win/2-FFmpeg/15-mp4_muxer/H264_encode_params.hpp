d0b55225ff171aa94b8503a543a830dcb351ec51
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-09-06 09:48:19 +0800

    Update H264_encode_params.hpp

--- code/win/2-FFmpeg/15-mp4_muxer/H264_encode_params.hpp
+++ code/win/2-FFmpeg/15-mp4_muxer/H264_encode_params.hpp
@@ -5,60 +5,62 @@
 #ifndef INC_15_MP4_MUXER_H264_ENCODE_PARAMS_HPP
 #define INC_15_MP4_MUXER_H264_ENCODE_PARAMS_HPP
 
-struct preset{
-    static inline constexpr auto first{"preset"};
+#define PARAMETERS(x) #x
+
+struct preset {
+    static inline constexpr auto first{PARAMETERS(preset)};
 protected:
     constexpr preset() = default;
 };
 
-struct preset_ultrafast_t final : public preset {
+struct preset_ultrafast_t final :  preset {
     constexpr preset_ultrafast_t() = default;
-    static inline constexpr auto second{"ultrafast"};
+    static inline constexpr auto second{PARAMETERS(ultrafast)};
 };
 
-struct preset_superfast_t final : public preset{
+struct preset_superfast_t final :  preset {
     constexpr preset_superfast_t() = default;
-    static inline constexpr auto second {"superfast"};
+    static inline constexpr auto second{PARAMETERS(superfast)};
 };
 
-struct preset_veryfast_t final : public preset{
+struct preset_veryfast_t final :  preset{
     constexpr preset_veryfast_t() = default;
-    static inline constexpr auto second{"veryfast"};
+    static inline constexpr auto second{PARAMETERS(veryfast)};
 };
 
-struct preset_faster_t final :public preset{
+struct preset_faster_t final : preset{
     constexpr preset_faster_t() = default;
-    static inline constexpr auto second{"faster"};
+    static inline constexpr auto second{PARAMETERS(faster)};
 };
 
-struct preset_fast_t final :public preset{
+struct preset_fast_t final : preset{
     constexpr preset_fast_t() = default;
-    static inline constexpr auto second{"medium"};
+    static inline constexpr auto second{PARAMETERS(fast)};
 };
 
-struct preset_medium_t final : public preset{
+struct preset_medium_t final :  preset{
     constexpr preset_medium_t() = default;
-    static inline constexpr auto second{"medium"};
+    static inline constexpr auto second{PARAMETERS(medium)};
 };
 
-struct preset_slow_t final : public preset{
+struct preset_slow_t final :  preset{
     constexpr preset_slow_t() = default;
-    static inline constexpr auto second {"slow"};
+    static inline constexpr auto second{PARAMETERS(slow)};
 };
 
-struct preset_slower_t final : public preset{
+struct preset_slower_t final :  preset{
     constexpr preset_slower_t() = default;
-    static inline constexpr auto second{"slower"};
+    static inline constexpr auto second{PARAMETERS(slower)};
 };
 
-struct preset_veryslow_t final : public preset{
+struct preset_veryslow_t final :  preset{
     constexpr preset_veryslow_t() = default;
-    static inline constexpr auto second{"veryslow"};
+    static inline constexpr auto second{PARAMETERS(veryslow)};
 };
 
-struct preset_placebo_t final :public preset{
+struct preset_placebo_t final : preset{
     constexpr preset_placebo_t() = default;
-    static inline constexpr auto second{"placebo"};
+    static inline constexpr auto second{PARAMETERS(placebo)};
 };
 
 static inline constexpr preset_ultrafast_t preset_ultrafast{};
@@ -73,50 +75,49 @@ static inline constexpr preset_veryslow_t preset_veryslow{};
 static inline constexpr preset_placebo_t preset_placebo{};
 
 struct tune {
-
-    static inline constexpr auto first{"tune"};
+    static inline constexpr auto first{PARAMETERS(tune)};
 protected:
     constexpr tune() = default;
 };
 
-struct tune_film_t final : public tune{
+struct tune_film_t final : tune{
     constexpr tune_film_t() = default;
-    static inline constexpr auto second{"film"};
+    static inline constexpr auto second{PARAMETERS(film)};
 };
 
-struct tune_animation_t final : public tune{
+struct tune_animation_t final : tune{
     constexpr tune_animation_t() = default;
-    static inline constexpr auto second{"animation"};
+    static inline constexpr auto second{PARAMETERS(animation)};
 };
 
-struct tune_grain_t final : public tune{
+struct tune_grain_t final : tune{
     constexpr tune_grain_t() = default;
-    static inline constexpr auto second{"grain"};
+    static inline constexpr auto second{PARAMETERS(grain)};
 };
 
-struct tune_stillimage_t final : public tune{
+struct tune_stillimage_t final : tune{
     constexpr tune_stillimage_t() = default;
-    static inline constexpr auto second{"stillimage"};
+    static inline constexpr auto second{PARAMETERS(stillimage)};
 };
 
-struct tune_psnr_t final : public tune{
+struct tune_psnr_t final : tune{
     constexpr tune_psnr_t() = default;
-    static inline constexpr auto second{"psnr"};
+    static inline constexpr auto second{PARAMETERS(psnr)};
 };
 
-struct tune_ssim_t final : public tune{
+struct tune_ssim_t final : tune{
     constexpr tune_ssim_t() = default;
-    static inline constexpr auto second{"ssim"};
+    static inline constexpr auto second{PARAMETERS(ssim)};
 };
 
-struct tune_fastdecode_t final : public tune{
+struct tune_fastdecode_t final : tune{
     constexpr tune_fastdecode_t() = default;
-    static inline constexpr auto second{"fastdecode"};
+    static inline constexpr auto second{PARAMETERS(fastdecode)};
 };
 
-struct tune_zerolatency_t final : public tune{
+struct tune_zerolatency_t final : tune{
     constexpr tune_zerolatency_t() = default;
-    static inline constexpr auto second{"zerolatency"};
+    static inline constexpr auto second{PARAMETERS(zerolatency)};
 };
 
 static inline constexpr tune_film_t tune_film{};
@@ -129,30 +130,29 @@ static inline constexpr tune_fastdecode_t tune_fastdecode{};
 static inline constexpr tune_zerolatency_t tune_zerolatency{};
 
 struct profile {
-
-    static inline constexpr auto first{"profile"};
+    static inline constexpr auto first{PARAMETERS(profile)};
 protected:
     constexpr profile() = default;
 };
 
-struct profile_baseline_t final : public profile{
+struct profile_baseline_t final :profile{
     constexpr profile_baseline_t() = default;
-    static inline constexpr auto second{"baseline"};
+    static inline constexpr auto second{PARAMETERS(baseline)};
 };
 
-struct profile_extended_t final : public profile{
+struct profile_extended_t final :profile{
     constexpr profile_extended_t() = default;
-    static inline constexpr auto second{"extended"};
+    static inline constexpr auto second{PARAMETERS(extended)};
 };
 
-struct profile_main_t final : public profile{
+struct profile_main_t final :profile{
     constexpr profile_main_t() = default;
-    static inline constexpr auto second{"main"};
+    static inline constexpr auto second{PARAMETERS(main)};
 };
 
-struct profile_high_t final : public profile{
+struct profile_high_t final :profile{
     constexpr profile_high_t() = default;
-    static inline constexpr auto second{"high"};
+    static inline constexpr auto second{PARAMETERS(high)};
 };
 
 static inline constexpr profile_baseline_t profile_baseline{};
