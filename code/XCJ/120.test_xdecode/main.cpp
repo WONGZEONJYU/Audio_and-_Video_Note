15a46434ca1389ec8b3adc77241e849d61537daf
WONGZEONJYU <61109848+WONGZEONJYU@users.noreply.github.com>
2024-09-10 16:27:02 +0800

    new add

---
+++ code/XCJ/120.test_xdecode/main.cpp
@@ -0,0 +1,237 @@
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavutil/pixdesc.h>
+}
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include "XAVPacket.hpp"
+#include "XAVFrame.hpp"
+#include "XHelper.hpp"
+#include "xvideo_view.hpp"
+
+using namespace std;
+static bool Decode(AVCodecContext *ctx,
+                   const XAVPacket_sptr &pkt,
+                   XAVFrame_sptr &frame,
+                   XAVFrame_sptr &hw_frame,
+                   int64_t &begin,
+                   int &count,XVideoView *);
+
+int main(const int argc,const char *argv[]) {
+
+    if (argc < 2){
+        PRINT_ERR_TIPS(GET_STR(argv miss!\n));
+        return -1;
+    }
+
+    ifstream ifs(argv[1],ios::binary);
+    AVCodecContext *codec_ctx{};
+    AVCodecParserContext *parser_ctx{};
+    XVideoView* view{};
+
+    const Destroyer d([&]{
+        cerr << "Destroyer\n";
+        avcodec_free_context(&codec_ctx);
+        av_parser_close(parser_ctx);
+        ifs.close();
+        delete view;
+    });
+
+    /**
+     * 创建显示对象
+     */
+    TRY_CATCH(CHECK_EXC(view = XVideoView::create()),return -1);
+
+    if (!ifs) {
+        PRINT_ERR_TIPS(GET_STR(h264_file open failed!));
+        return -1;
+    }
+
+    const auto codec_id{AV_CODEC_ID_H264};
+
+    /**
+     * 查找解码器
+     */
+    auto codec{avcodec_find_decoder(codec_id)};
+    if (!codec){
+        PRINT_ERR_TIPS(GET_STR(codec not found!));
+        return -1;
+    }
+
+    /**
+     * 分配解码器上下文
+     */
+    codec_ctx = avcodec_alloc_context3(codec);
+    if (!codec_ctx) {
+        PRINT_ERR_TIPS(GET_STR(avcodec_alloc_context3 failed!));
+        return -1;
+    }
+#if MACOS
+    const auto hw_type{AV_HWDEVICE_TYPE_VIDEOTOOLBOX};
+#else
+    const auto hw_type{AV_HWDEVICE_TYPE_DXVA2};
+#endif
+    /**
+     * 列出所有支持的硬件加速方式
+     */
+    for (int i {};;++i) {
+        auto config{avcodec_get_hw_config(codec,i)};
+        if (!config) {
+            break;
+        }
+        if (config->device_type){
+            cerr << av_hwdevice_get_type_name(config->device_type) << "\n";
+        }
+    }
+    AVBufferRef *hw_ctx{};
+    FF_ERR_OUT(av_hwdevice_ctx_create(&hw_ctx,hw_type,{},{},{}),return -1);
+    codec_ctx->hw_device_ctx = av_buffer_ref(hw_ctx);
+
+    /**
+     * 设置解码器线程数
+     */
+    codec_ctx->thread_count = static_cast<int>(thread::hardware_concurrency());
+
+    /**
+     * 打开解码器
+     */
+    FF_CHECK_ERR(avcodec_open2(codec_ctx,codec,nullptr),return -1);
+    /**
+     * 初始化解析器
+     */
+    parser_ctx = av_parser_init(codec_id);
+    if (!parser_ctx) {
+        PRINT_ERR_TIPS(GET_STR(parser_ctx is empty!));
+        return -1;
+    }
+
+    static constexpr auto DATA_SIZE{4096};
+    pmr::vector<uint8_t> read_data(DATA_SIZE,0);
+
+    XAVPacket_sptr packet;
+    XAVFrame_sptr frame;
+    XAVFrame_sptr hw_frame;
+    TRY_CATCH(CHECK_EXC(packet = new_XAVPacket()),return -1);
+    TRY_CATCH(CHECK_EXC(frame = new_XAVFrame()),return -1);
+    TRY_CATCH(CHECK_EXC(hw_frame = new_XAVFrame()),return -1);
+
+    int count{};
+    auto begin{XVideoView::Get_time_ms()};
+    while (!ifs.eof()) {
+
+        auto in_buffer{read_data.data()};
+        ifs.read(reinterpret_cast<char *>(in_buffer),static_cast<long long>(read_data.capacity()));
+        auto read_size {ifs.gcount()};
+
+        if (ifs.eof()){
+            ifs.clear();
+            ifs.seekg(0,ios::beg);
+        }
+
+        while (read_size) {
+            /**
+             * 解析H264裸流
+             */
+            const auto parser_len{av_parser_parse2(parser_ctx, codec_ctx,
+                                                   &packet->data, &packet->size,
+                                                   in_buffer, static_cast<int>(read_size),
+                                                   AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0)};
+            read_size -= parser_len;
+            in_buffer += parser_len;
+            if (packet->size) {
+                //cerr << "packet.size = " << packet->size << "\t";
+                /**
+                 * 解码
+                 */
+                if (Decode(codec_ctx,packet,frame,hw_frame,begin,count,view)) {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    {
+        /**
+         * 冲刷解析器,把缓存全部读取出来
+         */
+        av_parser_parse2(parser_ctx, codec_ctx,
+                         &packet->data, &packet->size,
+                         nullptr, 0, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+
+        if (packet->size) {
+            //cerr << "packet.size = " << packet->size << "\t";
+            cerr << "\n\nflush\n\n";
+            /**
+             * 冲刷解码器,把缓存的帧全部读取出来
+             */
+            Decode(codec_ctx, {}, frame,hw_frame,begin, count, view);
+        }
+    }
+
+    std::cerr << "\n\nparser and decode success!\n";
+
+    return 0;
+}
+
+static bool Decode(AVCodecContext *ctx,
+                   const XAVPacket_sptr &pkt,
+                   XAVFrame_sptr &frame,
+                   XAVFrame_sptr &hw_frame,
+                   int64_t &begin,
+                   int &count,
+                   XVideoView *view){
+
+    static bool is_view_init{};
+
+    /**
+     * 发送未压缩的包到解码器
+     */
+    auto ret{avcodec_send_packet(ctx,pkt.get())};
+    if (0 != ret || AVERROR(EAGAIN) != ret) {
+        FF_ERR_OUT(ret, return {});
+    }
+
+    while (true) {
+        /**
+         * 读取解码后的帧
+         */
+        if (avcodec_receive_frame(ctx,frame.get()) < 0) {
+            return false;
+        }
+
+        auto p_frame{frame};
+        if (ctx->hw_device_ctx){
+            FF_ERR_OUT(av_hwframe_transfer_data(hw_frame.get(),frame.get(),0), return {});
+            p_frame = hw_frame;
+        }
+
+        ++count;
+        cerr << av_get_pix_fmt_name(static_cast<AVPixelFormat>(p_frame->format)) << "\n";
+
+        const auto curr_time{XVideoView::Get_time_ms()};
+        if (curr_time - begin >= 1000LL) {
+            std::cerr << "fps :" << count << "\n";
+            count = 0;
+            begin = curr_time;
+        }
+
+        if (!is_view_init) {
+            /**
+             * 用第一帧来初始化显示
+             */
+            is_view_init = true;
+            view->Init(p_frame->width,p_frame->height,static_cast<XVideoView::Format>(p_frame->format));
+        }
+
+        /**
+         * 显示一帧画面
+         */
+        view->DrawFrame(p_frame);
+        if (view->Is_Exit_Window()){
+            return true;
+        }
+    }
+}
---
+++ code/XCJ/120.test_xdecode/xencode.hpp
@@ -0,0 +1,143 @@
+//
+// Created by Administrator on 2024/9/7.
+//
+
+#ifndef INC_116_TEST_XENCODE_XENCODE_HPP
+#define INC_116_TEST_XENCODE_XENCODE_HPP
+
+#include "XHelper.hpp"
+#include <mutex>
+#include <string>
+#include <vector>
+#include "Encode_Params.hpp"
+
+struct AVCodecContext;
+class XAVFrame;
+class XAVPacket;
+
+using XAVFrame_sptr = typename std::shared_ptr<XAVFrame>;
+using XAVPacket_sptr = typename std::shared_ptr<XAVPacket>;
+
+class XEncode {
+
+public:
+    /**
+     * 创建编码上下文
+     * @param codec_id 对应ffmpeg
+     * @return AVCodecContext* or nullptr
+     */
+    static AVCodecContext *Create(const int &codec_id);
+
+    /**
+     * 设置编码器上下文 上下文传递到对象中,资源由XEncode维护
+     * 加锁 线程安全
+     * 如果m_codec_ctx不为nullptr,则先清理资源
+     * @param ctx
+     */
+    void set_codec_ctx(AVCodecContext *ctx);
+
+    /**
+     * 设置编码器参数,线程安全
+     * @param key
+     * @param val
+     * @return ture or false
+     */
+
+    bool SetOpt(const std::string &key,const std::string &val);
+    bool SetOpt(const std::string &key,const int64_t &val);
+
+    /**
+     * 设置Preset参数,线程安全
+     * @tparam T
+     * @param p
+     * @return ture or false
+     */
+    template<typename T>
+    std::enable_if_t<std::is_base_of_v<PRESET,T>,bool> Set_Preset(const T &p){
+        return SetOpt(p.first,p.second);
+    }
+
+    /**
+     * 设置Tune参数,线程安全
+     * @tparam T
+     * @param t
+     * @return ture or false
+     */
+    template<typename T>
+    std::enable_if_t<std::is_base_of_v<TUNE,T>,bool > Set_Tune(const T &t){
+        return SetOpt(t.first,t.second);
+    }
+
+    /**
+     * 设置Profile参数,线程安全
+     * @tparam T
+     * @param p
+     * @return ture or false
+     */
+    template<typename T>
+    std::enable_if_t<std::is_base_of_v<PROFILE,T>,bool> Set_Profile(const T &p){
+        return SetOpt(p.first,p.second);
+    }
+
+    /**
+     * 设置qp值
+     * @param qp
+     * @return ture or false
+     */
+    bool Set_Qp(const QP &qp);
+
+    /**
+     * 设置NAL_HRD,线程安全
+     * @tparam T
+     * @param n
+     * @return ture or false
+     */
+    template<typename T>
+    std::enable_if_t<std::is_base_of_v<NAL_HRD,T>,bool > Set_NAL_HRD(const T &n){
+        return SetOpt(n.first,n.second);
+    }
+
+    /**
+     * 设置crf,线程安全
+     * @param crf
+     * @return ture or false
+     */
+    bool Set_CRF(const CRF &crf);
+
+    /**
+     * 打开编码器,线程安全
+     * @return ture or false
+     */
+    bool Open();
+
+    /**
+     * 编码数据,线程安全,调用一次,创建一次AVPacket对象
+     * AVPacket,AVFrame让智能指针自动维护,无需用户干预
+     * @param frame
+     * @return XAVPacket_sptr or nullptr
+     */
+    XAVPacket_sptr Encode(const XAVFrame_sptr &frame);
+
+    /**
+     * 冲刷编码器
+     * @return std::vector<XAVPacket_sptr>
+     */
+    std::vector<XAVPacket_sptr> Flush();
+
+    /**
+     * 分配AVFrame对象
+     * @return XAVFrame_sptr or nullptr
+     */
+    XAVFrame_sptr Alloc_AVFrame();
+
+private:
+    AVCodecContext *m_codec_ctx{};
+    std::mutex m_mux;
+
+public:
+    explicit XEncode() = default;
+    virtual ~XEncode();
+    X_DISABLE_COPY_MOVE(XEncode)
+};
+
+#endif
