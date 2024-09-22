0b69b58ae961adb31f7fb5d89e2feb4d1a4a804b
fuck_if <wongjy13005151320@outlook.com>
2024-09-23 01:19:53 +0800

    new add

---
+++ code/XCJ/124.test_xformat/xmux.cpp
@@ -0,0 +1,65 @@
+//
+// Created by wong on 2024/9/22.
+//
+#define check_ctx \
+std::unique_lock locker(m_mux);do{\
+if(!m_fmt_ctx) {\
+PRINT_ERR_TIPS(GET_STR(ctx is empty));\
+return {};}}while(false)
+
+extern "C"{
+#include <libavformat/avformat.h>
+}
+#include "xmux.hpp"
+#include "xavpacket.hpp"
+
+AVFormatContext *XMux::Open(const std::string &url) {
+
+    if (url.empty()){
+        PRINT_ERR_TIPS(GET_STR(url is empty!));
+        return {};
+    }
+
+    AVFormatContext *c{};
+    FF_ERR_OUT(avformat_alloc_output_context2(&c, nullptr, nullptr,url.c_str()),
+               return {});
+
+    AVStream *vs,*as;
+
+    TRY_CATCH(CHECK_NULLPTR(vs = avformat_new_stream(c, nullptr)),
+              avformat_free_context(c);return {});
+    vs->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
+
+    TRY_CATCH(CHECK_NULLPTR(as = avformat_new_stream(c, nullptr)),
+              avformat_free_context(c);return {});
+    as->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
+
+    FF_ERR_OUT(avio_open(&c->pb,url.c_str(),AVIO_FLAG_WRITE),
+               avformat_free_context(c);return {});
+
+    return c;
+}
+
+bool XMux::WriteHead(){
+    check_ctx;
+    FF_ERR_OUT(avformat_write_header(m_fmt_ctx, nullptr),return {});
+    av_dump_format(m_fmt_ctx,0, m_fmt_ctx->url,1);
+    return true;
+}
+
+bool XMux::Write(XAVPacket *packet){
+    if (!packet){
+        PRINT_ERR_TIPS(GET_STR(packet is empty!));
+        return {};
+    }
+    check_ctx;
+    FF_ERR_OUT(av_interleaved_write_frame(m_fmt_ctx,packet),return {});
+    return true;
+}
+
+bool XMux::WriteEnd(){
+    check_ctx;
+    av_interleaved_write_frame(m_fmt_ctx, nullptr);
+    FF_ERR_OUT(av_write_trailer(m_fmt_ctx),return {});
+    return true;
+}
---
+++ code/XCJ/124.test_xformat/xmux.hpp
@@ -0,0 +1,43 @@
+//
+// Created by wong on 2024/9/22.
+//
+
+#ifndef INC_124_TEST_XFORMAT_XMUX_HPP
+#define INC_124_TEST_XFORMAT_XMUX_HPP
+
+#include "xformat.hpp"
+
+class XAVPacket;
+
+class XMux : public XFormat{
+
+public:
+    static AVFormatContext *Open(const std::string &url);
+
+    /**
+     * 写入头部信息
+     * @return ture or false
+     */
+    bool WriteHead();
+
+    /**
+     * 写入包数据
+     * @param packet
+     * @return ture or false
+     */
+    bool Write(XAVPacket *packet);
+
+    /**
+     * 写入尾部信息
+     * @return ture or false
+     */
+    bool WriteEnd();
+
+public:
+    explicit XMux() = default;
+    X_DISABLE_COPY_MOVE(XMux)
+
+};
+
+
+#endif
