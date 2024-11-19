3e0ce32cb4ac8bd8be09645addfb0221fed32b3c
fuck_if <wongjy13005151320@outlook.com>
2024-11-19 11:37:07 +0800

    Update xaduio_parameters.hpp

--- code/xlib/xaduio_parameters.hpp
+++ code/xlib/xaduio_parameters.hpp
@@ -45,27 +45,37 @@
 #define AUDIO_F32SYS    AUDIO_F32MSB
 #endif
 
-#define ENUM_AUDIO_FMT(name) enum class name##_##AudioFormats
+#define ENUM_AUDIO_FMT(name) enum name##_##AudioFormats
 #define SET_FMT_VAL(sub_enum,name,value) sub_enum##_##name = value
 #define GET_FMT_VAL(enum_name) enum_name##_##AudioFormats
-
-#define XAUDIO_MAP(sub_enum,arr_name) \
-static constexpr std::pair<ENUM_AUDIO_FMT(XAudio),ENUM_AUDIO_FMT(sub_enum)> arr_name[]
 
 #include <type_traits>
 #include <array>
 
+ENUM_AUDIO_FMT(XAudio)  {
+    SET_FMT_VAL(XAudio,Unknown,-1),
+    SET_FMT_VAL(XAudio,S8_FMT,1),
+    SET_FMT_VAL(XAudio,U8_FMT,2),
+    SET_FMT_VAL(XAudio,S16_FMT,3),
+    SET_FMT_VAL(XAudio,U16_FMT,4),
+    SET_FMT_VAL(XAudio,S32_FMT,5),
+    SET_FMT_VAL(XAudio,U32_FMT,6),
+    SET_FMT_VAL(XAudio,S64_FMT,7),
+    SET_FMT_VAL(XAudio,U64_FMT,8),
+    SET_FMT_VAL(XAudio,FLOAT_FMT,9),
+    SET_FMT_VAL(XAudio,DOUBLE_FMT,10),
+};
 
-ENUM_AUDIO_FMT(Qt) : int {
-    Unknown = 0,
+ENUM_AUDIO_FMT(Qt)  {
+    SET_FMT_VAL(Qt,Unknown,0),
     SET_FMT_VAL(Qt,UInt8,1),
     SET_FMT_VAL(Qt,Int16,2),
     SET_FMT_VAL(Qt,Int32,3),
     SET_FMT_VAL(Qt,Float,4),
 };
 
-ENUM_AUDIO_FMT(SDL) : int {
-    Unknown = -1,
+ENUM_AUDIO_FMT(SDL)  {
+    SET_FMT_VAL(SDL,Unknown,-1),
     SET_FMT_VAL(SDL,AUDIO_U8,AUDIO_U8),
     SET_FMT_VAL(SDL,AUDIO_S8,AUDIO_S8),
 
@@ -91,19 +101,6 @@ ENUM_AUDIO_FMT(SDL) : int {
     SET_FMT_VAL(SDL,AUDIO_F32SYS,AUDIO_F32SYS),
 };
 
-ENUM_AUDIO_FMT(XAudio) : int {
-    Unknown = -1,
-    SET_FMT_VAL(XAudio,S8_FMT,1),
-    SET_FMT_VAL(XAudio,U8_FMT,2),
-    SET_FMT_VAL(XAudio,S16_FMT,3),
-    SET_FMT_VAL(XAudio,U16_FMT,4),
-    SET_FMT_VAL(XAudio,S32_FMT,5),
-    SET_FMT_VAL(XAudio,U32_FMT,6),
-    SET_FMT_VAL(XAudio,S64_FMT,7),
-    SET_FMT_VAL(XAudio,U64_FMT,8),
-    SET_FMT_VAL(XAudio,FLOAT_FMT,9),
-    SET_FMT_VAL(XAudio,DOUBLE_FMT,10),
-};
 
 struct XAudioSpec {
   int m_freq {44100};
@@ -111,33 +108,16 @@ struct XAudioSpec {
   int m_channels{2},m_samples{1024};
 };
 
-static inline ENUM_AUDIO_FMT(Qt) xaudio_fmt_to_qt_audio_format(const ENUM_AUDIO_FMT(XAudio) &f) {
+#define XAUDIO_MAP(sub_enum,arr_name) \
+static constexpr std::pair<ENUM_AUDIO_FMT(XAudio),ENUM_AUDIO_FMT(sub_enum)> arr_name[]
+
+static ENUM_AUDIO_FMT(Qt) to_qt_audio_format(const ENUM_AUDIO_FMT(XAudio) &f) {
 
     XAUDIO_MAP(Qt,list){
-        {GET_FMT_VAL(XAudio)::XAudio_U8_FMT,GET_FMT_VAL(Qt)::Qt_UInt8},
-        {GET_FMT_VAL(XAudio)::XAudio_S16_FMT,GET_FMT_VAL(Qt)::Qt_Int16},
-        {GET_FMT_VAL(XAudio)::XAudio_S32_FMT,GET_FMT_VAL(Qt)::Qt_Int32},
-        {GET_FMT_VAL(XAudio)::XAudio_FLOAT_FMT,GET_FMT_VAL(Qt)::Qt_Float},
-    };
-
-    for (const auto &[first, second] : list) {
-        if (first == f) {
-            return second;
-        }
-    }
-
-    return GET_FMT_VAL(Qt)::Unknown;
-}
-
-static inline ENUM_AUDIO_FMT(SDL) xaudio_fmt_to_sdl_audio_format(const ENUM_AUDIO_FMT(XAudio) &f) {
-
-    XAUDIO_MAP(SDL,list) {
-        {GET_FMT_VAL(XAudio)::XAudio_S8_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S8},
-        {GET_FMT_VAL(XAudio)::XAudio_U8_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_U8},
-        {GET_FMT_VAL(XAudio)::XAudio_S16_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S16SYS},
-        {GET_FMT_VAL(XAudio)::XAudio_U16_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_U16SYS},
-        {GET_FMT_VAL(XAudio)::XAudio_S32_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S32SYS},
-        {GET_FMT_VAL(XAudio)::XAudio_FLOAT_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_F32SYS},
+            {GET_FMT_VAL(XAudio)::XAudio_U8_FMT,GET_FMT_VAL(Qt)::Qt_UInt8},
+            {GET_FMT_VAL(XAudio)::XAudio_S16_FMT,GET_FMT_VAL(Qt)::Qt_Int16},
+            {GET_FMT_VAL(XAudio)::XAudio_S32_FMT,GET_FMT_VAL(Qt)::Qt_Int32},
+            {GET_FMT_VAL(XAudio)::XAudio_FLOAT_FMT,GET_FMT_VAL(Qt)::Qt_Float},
     };
 
     for (const auto &[first,second] : list) {
@@ -146,7 +126,27 @@ static inline ENUM_AUDIO_FMT(SDL) xaudio_fmt_to_sdl_audio_format(const ENUM_AUDI
         }
     }
 
-    return GET_FMT_VAL(SDL)::Unknown;
+    return GET_FMT_VAL(Qt)::Qt_Unknown;
+}
+
+static auto to_sdl_audio_format(const ENUM_AUDIO_FMT(XAudio) &f) {
+
+    XAUDIO_MAP(SDL,list) {
+            {GET_FMT_VAL(XAudio)::XAudio_S8_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S8},
+            {GET_FMT_VAL(XAudio)::XAudio_U8_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_U8},
+            {GET_FMT_VAL(XAudio)::XAudio_S16_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S16SYS},
+            {GET_FMT_VAL(XAudio)::XAudio_U16_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_U16SYS},
+            {GET_FMT_VAL(XAudio)::XAudio_S32_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_S32SYS},
+            {GET_FMT_VAL(XAudio)::XAudio_FLOAT_FMT,GET_FMT_VAL(SDL)::SDL_AUDIO_F32SYS},
+    };
+
+    for (const auto &[first,second] : list) {
+        if (first == f) {
+            return second;
+        }
+    }
+
+    return GET_FMT_VAL(SDL)::SDL_Unknown;
 }
 
 #undef ENUM_AUDIO_FMT
