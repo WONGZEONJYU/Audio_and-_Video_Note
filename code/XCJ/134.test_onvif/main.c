b39f12528ed96e8994f488af53e6f4d507fc4c48
fuck_if <wongjy13005151320@outlook.com>
2024-12-10 00:00:46 +0800

    m

--- code/XCJ/134.test_onvif/main.c
+++ code/XCJ/134.test_onvif/main.c
@@ -1,11 +1,93 @@
 #include "soapH.h"
 #include "wsdd.nsmap"
+#include "wsaapi.h"
 
-int main(const int argc,const char * const argv[]) {
-    (void)argc;
-    (void)argv;
+#define X_TO "urn:schemas-xmlsoap-org:ws:2005:04:discovery"
+#define X_ACT "http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe"
+#define X_IEAM ""
+#define X_TYPE "tdn:NetworkVideoTransmitter tds:Device"  //设备类型
+#define X_ADDR "soap.udp://239.255.255.250:3702"
+int main(const int argc,const char *argv[])
+{
+    (void )argc,(void )argv;
+    //for(;;){
+    /////////////////////////////////////////////////////////
+    /// 初始化soap
+    struct soap* soap;
+    int timeout_sec = 1;
+    soap = soap_new();
 
-    struct soap * soap = soap_new();
-    soap_delete(soap,NULL);
+    //设置命名空间
+    soap_set_namespaces(soap, namespaces);
+
+    //设置超时时间
+    soap->recv_timeout = timeout_sec;
+    soap->send_timeout = timeout_sec;
+    soap->connect_timeout = timeout_sec;
+
+    //设置xml soap消息编码格式 utf
+    soap_set_mode(soap, SOAP_C_UTFSTRING);
+
+
+    ///////////////////////////////////////////////////
+    /// 初始化消息头
+    struct SOAP_ENV__Header* header;
+    header = (struct SOAP_ENV__Header*)soap_malloc(soap,
+                                                   sizeof( struct SOAP_ENV__Header));
+    memset(header, 0, sizeof(struct SOAP_ENV__Header));
+    soap_default_SOAP_ENV__Header(soap, header);
+    header->wsa5__MessageID = (char*)soap_wsa_rand_uuid(soap); // 消息编号，唯一随机uuid
+    //"urn:schemas-xmlsoap-org:ws:2005:04:discovery"
+    header->wsa5__To = soap_malloc(soap, strlen(X_TO) + 1);
+
+    //C4996	_CRT_SECURE_NO_WARNINGS
+    strcpy(header->wsa5__To, X_TO);
+    //"http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe"
+    header->wsa5__Action = soap_malloc(soap, strlen(X_ACT) + 1);
+    strcpy(header->wsa5__Action, X_ACT);
+    soap->header = header;
+
+
+    //设定探测类型
+    struct wsdd__ScopesType* scope;
+    scope = soap_malloc(soap, sizeof(wsdd__ScopesType));
+    memset(scope,0,sizeof (struct wsdd__ScopesType));
+    //设定寻找设备的范围
+    soap_default_wsdd__ScopesType(soap, scope);
+    //设备范围
+    scope->__item = soap_malloc(soap, strlen(X_IEAM) + 1);
+    strcpy(scope->__item, X_IEAM);
+
+    //设定寻找设备的类型
+    //"tdn:NetworkVideoTransmitter tds:Device"
+    struct wsdd__ProbeType probe;
+    memset(&probe, 0, sizeof(probe));
+    soap_default_wsdd__ProbeType(soap, &probe);
+    probe.Scopes = scope;
+    probe.Types = soap_malloc(soap, strlen(X_TYPE)+1);
+    strcpy(probe.Types, X_TYPE);
+
+    //发送广播消息探测设备
+    // "soap.udp://239.255.255.250:3702"
+    int re = soap_send___wsdd__Probe(soap, X_ADDR, nullptr, &probe);
+
+
+    while (SOAP_OK == re){
+        struct __wsdd__ProbeMatches resp; //接收探测结果
+        re = soap_recv___wsdd__ProbeMatches(soap, &resp);
+        if (SOAP_OK == re){
+            printf("#\n");
+            //char *device_service = resp.wsdd__ProbeMatches->ProbeMatch->XAddrs;
+            //printf("device_service:[%s]\n", device_service);
+        }
+    }
+    ////////////////////////////////////////////////////
+    ///清理soap
+    printf("begin Clear soap!\n");
+    soap_destroy(soap);     // c++析构
+    soap_end(soap);         //清理临时空间
+    soap_done(soap);        //关闭通信，移除回调
+    soap_free(soap);        //删除soap对象空间
+    // }
     return 0;
 }
--- code/XCJ/134.test_onvif_c++/main.cpp
+++ code/XCJ/134.test_onvif_c++/main.cpp
@@ -39,15 +39,22 @@ int main(const int argc, const char * argv[]) {
     soap_default_SOAP_ENV__Header(soap_,header);
 
     header->wsa5__MessageID = const_cast<char*>(soap_wsa_rand_uuid(soap_)); //消息编号,唯一随机uuid
-
-    header->wsa5__To = static_cast<decltype(header->wsa5__To)>(soap_malloc(soap_,strlen(X_TO) + 1));
-    /*X_TO = urn:schemas-xmlsoap-org:ws:2005:04:discovery */
-    strcpy(reinterpret_cast<char*>(header->wsa5__MessageID),X_TO);
+    //header->wsa5__MessageID = const_cast<char *>("urn:uuid:1419d68a-1dd2-11b2-a105-F000010A654C");
+    cout << "header->wsa5__MessageID : " << header->wsa5__MessageID << "\n";
 
 
-    header->wsa5__Action = static_cast<decltype(header->wsa5__Action)>(soap_malloc(soap_,strlen(X_ACT) + 1));
+    header->wsa5__To = static_cast<decltype(header->wsa5__To)>(soap_malloc(soap_,size(X_TO) + 1));
+    /* X_TO = urn:schemas-xmlsoap-org:ws:2005:04:discovery */
+    copy_n(X_TO, size(X_TO),header->wsa5__To);
+    header->wsa5__To[size(X_TO)] = 0;
+    cout << "header->wsa5__To : " << header->wsa5__To << "\n";
+
+
+    header->wsa5__Action = static_cast<decltype(header->wsa5__Action)>(soap_malloc(soap_,size(X_ACT) + 1));
     /*X_ACT = http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe */
-    strcpy((reinterpret_cast<char*>(header->wsa5__Action)),X_ACT);
+    copy_n(X_ACT, size(X_ACT),header->wsa5__Action);
+    header->wsa5__Action[size(X_ACT)] = 0;
+    cout << "header->wsa5__Action : " << header->wsa5__Action << "\n";
 
     soap_->header = header;
 
@@ -59,9 +66,11 @@ int main(const int argc, const char * argv[]) {
     //设定寻找设备的范围
     soap_default_wsdd__ScopesType(soap_,scopes);
     //设备范围
-    scopes->__item = static_cast<decltype(scopes->__item)>(soap_malloc(soap_,strlen(X_ITEM) + 1));
-    strcpy(scopes->__item,X_ITEM);
-
+    scopes->__item = static_cast<decltype(scopes->__item)>(soap_malloc(soap_,size(X_ITEM) + 1));
+    //strcpy(scopes->__item,X_ITEM);
+    copy_n(X_ITEM, size(X_ITEM),scopes->__item);
+    scopes->__item[size(X_ITEM)] = 0;
+    cout << "scopes->__item : " << scopes->__item << "\n";
 
     /*设定寻找设备的类型*/
     /* tdn:NetworkVideoTransmitter tds:Device */
@@ -69,21 +78,22 @@ int main(const int argc, const char * argv[]) {
     soap_default_wsdd__ProbeType(soap_,&probe_);
     probe_.Scopes = scopes;
     probe_.Types = static_cast<decltype(probe_.Types)>(soap_malloc(soap_,strlen(X_TYPE) + 1));
-    strcpy(probe_.Types,X_TYPE);
-    // copy_n(X_TYPE,size(X_TYPE),probe_.Types);
-    // probe_.Types[size(X_TYPE)] = 0;
+    //strcpy(probe_.Types,X_TYPE);
+    copy_n(X_TYPE,size(X_TYPE),probe_.Types);
+    probe_.Types[size(X_TYPE)] = 0;
+    cout << "probe_.Types : " << probe_.Types << "\n";
 
     /*发送广播消息探测设备*/
     /* X_ADDR soap.udp://239.255.255.250:3702 */
     auto re{soap_send___wsdd__Probe(soap_,X_ADDR,nullptr,&probe_)};
 
-    __wsdd__ProbeMatches resp{};
     while (SOAP_OK == re) {
-        fill_n(reinterpret_cast<uint8_t*>(&resp),sizeof(resp),0);
+        __wsdd__ProbeMatches resp{};
         re = soap_recv___wsdd__ProbeMatches(soap_,&resp);
         if (SOAP_OK == re) {
-            cout << "#" << endl;
-            cerr << resp.wsdd__ProbeMatches->ProbeMatch->XAddrs << "\n";
+            //cout << "#" << endl;
+            const auto addr{resp.wsdd__ProbeMatches ? resp.wsdd__ProbeMatches->ProbeMatch->XAddrs : nullptr};
+            cerr << (addr ? addr : "none") << "\n";
         }
     }
 
