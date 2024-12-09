67e1ef2f2d232c03084f0479be571abbeda8377e
fuck_if <wongjy13005151320@outlook.com>
2024-12-09 15:27:43 +0800

    Update main.cpp

--- code/XCJ/134.test_onvif_c++/main.cpp
+++ code/XCJ/134.test_onvif_c++/main.cpp
@@ -4,13 +4,8 @@
 
 int main(const int argc, const char * argv[]) {
     (void)argc, (void)argv;
-
-    (void)argc;
-    (void)argv;
-
     soap *soap {};
     soap = soap_new();
     soap_delete(soap,nullptr);
     return 0;
 }
-
